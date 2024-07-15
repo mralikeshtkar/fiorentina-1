e9f5683c4bb1dd859722bcc90e3ecd085b7a0bba
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-07-15 09:14:04 +0200

    Update create.blade.php

--- resources/views/ads/create.blade.php
+++ resources/views/ads/create.blade.php
@@ -1,8 +1,8 @@
 @extends(BaseHelper::getAdminMasterLayoutTemplate())
 @section('content')
 
-    @foreach ($ads as $ad)
-        {{ $ad->name }}
-    @endforeach
+{{--    --}}@foreach ($ads as $ad)--}}
+{{--        {{ $ad->name }}--}}
+{{--    @endforeach
 
 @stop
