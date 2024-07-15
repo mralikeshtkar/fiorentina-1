c49c0687ac15f6fd17cd67286db0b5b77a3de199
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-07-15 09:25:21 +0200

    Update create.blade.php

--- resources/views/ads/create.blade.php
+++ resources/views/ads/create.blade.php
@@ -1,4 +1,6 @@
 @extends(BaseHelper::getAdminMasterLayoutTemplate())
+
+
 @section('content')
     <div id="poststuff">
         <div id="post-body" class="metabox-holder columns-2">
@@ -13,31 +15,71 @@
 
             <div id="postbox-container-1" class="postbox-container">
                 <div id="side-sortables" class="meta-box-sortables ui-sortable">
-{{--                    @include('partials.publish-box')--}}
+                    <div id="submitdiv" class="postbox ">
+                        <div class="postbox-header">
+                            <h2 class="hndle ui-sortable-handle">Pubblica</h2>
+                        </div>
+                        <div class="inside">
+                            <div class="submitbox" id="submitpost">
+                                <div id="minor-publishing">
+                                    <div style="display:none;">
+                                        <input type="submit" name="save" id="save" class="button" value="Salva">
+                                    </div>
+                                    <div id="minor-publishing-actions">
+                                        <div id="save-action">
+                                            <input type="submit" name="save" id="save-post" value="Salva bozza" class="button">
+                                            <span class="spinner"></span>
+                                        </div>
+                                    </div>
+                                    <div id="misc-publishing-actions">
+                                        <div class="misc-pub-section misc-pub-post-status">
+                                            Stato:
+                                            <span id="post-status-display">
+                                            Bozza
+                                        </span>
+                                            <a href="#post_status" class="edit-post-status hide-if-no-js" role="button">
+                                                <span aria-hidden="true">Modifica</span>
+                                                <span class="screen-reader-text">Modifica stato</span>
+                                            </a>
+                                            <div id="post-status-select" class="hide-if-js">
+                                                <input type="hidden" name="hidden_post_status" id="hidden_post_status" value="draft">
+                                                <select name="post_status" id="post_status">
+                                                    <option value="pending">In attesa di revisione</option>
+                                                    <option selected="selected" value="draft">Bozza</option>
+                                                </select>
+                                                <a href="#post_status" class="save-post-status hide-if-no-js button">OK</a>
+                                                <a href="#post_status" class="cancel-post-status hide-if-no-js button-cancel">Annulla</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div id="postbox-container-2" class="postbox-container">
-{{--                @include('partials.ad-main-box')--}}
+                <div id="normal-sortables" class="meta-box-sortables ui-sortable">
+                    <div id="ad-main-box" class="postbox ">
+                        <div class="postbox-header">
+                            <h2 class="hndle ui-sortable-handle">Tipo Annuncio: Testo semplice e Codice</h2>
+                        </div>
+                        <div class="inside">
+                            <ul id="ad-main-box-notices" class="advads-metabox-notices"></ul>
+                            <ul id="advanced-ad-type">
+                                <li class="advanced-ads-type-list-plain">
+                                    <input type="radio" name="advanced_ad[type]" id="advanced-ad-type-plain" value="plain" checked="checked">
+                                    <label for="advanced-ad-type-plain">Testo semplice e Codice</label>
+                                </li>
+                                {{-- Additional ad types here --}}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <br class="clear">
     </div>
+@endsection
 
-
-{{-- Include partials for sections like publishing actions --}}
-{{--@section('partials')--}}
-{{--    --}}{{-- Publish Box --}}
-{{--    @component('components.publish-box')--}}
-{{--        --}}{{-- Dynamic data can be passed to components like this --}}
-{{--        @slot('status', $postStatus ?? 'Draft')--}}
-{{--    @endcomponent--}}
-
-{{--    --}}{{-- Ad Main Box --}}
-{{--    @component('components.ad-main-box')--}}
-{{--        @slot('type', $adType ?? 'Standard')--}}
-{{--    @endcomponent--}}
-
-
-
-@stop
