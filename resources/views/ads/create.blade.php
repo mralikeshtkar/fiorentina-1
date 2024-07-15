47a5fc16f102758fe00964da6ba63a0438527e57
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-07-15 09:20:15 +0200

    Update create.blade.php

--- resources/views/ads/create.blade.php
+++ resources/views/ads/create.blade.php
@@ -1,5 +1,42 @@
 @extends(BaseHelper::getAdminMasterLayoutTemplate())
 @section('content')
+    <div id="poststuff">
+        <div id="post-body" class="metabox-holder columns-2">
+            <div id="post-body-content">
+                <div id="titlediv">
+                    <div id="titlewrap">
+                        <label class="" id="title-prompt-text" for="title">Aggiungi titolo</label>
+                        <input type="text" name="post_title" size="30" value="{{ old('post_title') }}" id="title" spellcheck="true" autocomplete="off">
+                    </div>
+                </div>
+            </div>
+
+            <div id="postbox-container-1" class="postbox-container">
+                <div id="side-sortables" class="meta-box-sortables ui-sortable">
+                    @include('partials.publish-box')
+                </div>
+            </div>
+
+            <div id="postbox-container-2" class="postbox-container">
+                @include('partials.ad-main-box')
+            </div>
+        </div>
+        <br class="clear">
+    </div>
+@endsection
+
+{{-- Include partials for sections like publishing actions --}}
+@section('partials')
+    {{-- Publish Box --}}
+    @component('components.publish-box')
+        {{-- Dynamic data can be passed to components like this --}}
+        @slot('status', $postStatus ?? 'Draft')
+    @endcomponent
+
+    {{-- Ad Main Box --}}
+    @component('components.ad-main-box')
+        @slot('type', $adType ?? 'Standard')
+    @endcomponent
 
 
 
