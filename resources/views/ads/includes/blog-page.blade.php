8f15bc2fabd499e7a07abc8cf0de7ac0ae57389c
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-07-29 12:11:30 +0200

    Update blog-page.blade.php

--- resources/views/ads/includes/blog-page.blade.php
+++ resources/views/ads/includes/blog-page.blade.php
@@ -1,11 +1,14 @@
+
 @if(isset($ads) && $ads)
     <div class="row mx-0">
         @foreach($ads as $ad)
             <div class="col-12 col-md-6 mb-4">
-                <a href="{{ $ad->getLink() }}" class="d-block w-100">
+                <a href="" class="d-block w-100">
                     <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid">
                 </a>
             </div>
         @endforeach
     </div>
 @endif
+
+
