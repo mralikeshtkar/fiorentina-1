75fd6c6e90e9ea346ffd8cffd8a1080fd418d847
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-07-29 12:17:52 +0200

    Update blog-page.blade.php

--- resources/views/ads/includes/blog-page.blade.php
+++ resources/views/ads/includes/blog-page.blade.php
@@ -2,7 +2,7 @@
 @if(isset($ads) && $ads)
     <div class="row mx-0">
         @foreach($ads as $ad)
-            <div class="col-12 ">
+            <div>
 
                     <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid">
 
