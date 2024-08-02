fd1572c57e1f1a3dd69ba27f8914e431dbbd6f3b
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-08-02 13:39:12 +0200

    Update dblog-p.blade.php

--- resources/views/ads/includes/dblog-p.blade.php
+++ resources/views/ads/includes/dblog-p.blade.php
@@ -1,6 +1,14 @@
-<div>
-    dfdgdfgd
-</div>
+@if(isset($ad) && $ad)
+    <div class="row mx-0">
+
+            <div class="col-12 col-md-6 mb-4">
+                <a href="" class="d-block">
+                    <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid" style="width: 100%; height: auto;">
+                </a>
+            </div>
+
+    </div>
+@endif
 
 
 
