8314c18f7d58704b3ea50ac75a802796d3f522e0
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-08-19 10:59:21 +0200

    54Y

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,26 +24,26 @@ class AppServiceProvider extends ServiceProvider
         set_time_limit(900); // Sets the maximum execution time to 15 minutes
 
         view()->composer('ads.includes.main-page',function (View $view){
-            $view->with('ads',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::DESKTOP_MAIN_PAGE)->get());
+            $view->with('ads',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::GROUP_MAIN_PAGE)->get());
         });
         view()->composer('ads.includes.blog-page',function (View $view){
-            $view->with('ads',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::DESKTOP_BLOG_PAGE)->get());
+            $view->with('ads',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::GROUP_BLOG_PAGE)->get());
         });
         view()->composer('ads.includes.dblog-title',function (View $view){
-            $view->with('ads',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::DESKTOP_DBLOG_TITLE)->get());
+            $view->with('ads',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::GROUP_DBLOG_TITLE)->get());
         });
         view()->composer('ads.includes.dblog-author',function (View $view){
-            $view->with('ads',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::DESKTOP_DBLOG_AUTHOR)->get());
+            $view->with('ads',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::GROUP_DBLOG_AUTHOR)->get());
         });
         view()->composer('ads.includes.dblog-P1',function (View $view){
-            $view->with('ads',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::DESKTOP_DBLOG_P1)->first());
+            $view->with('ads',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::GROUP_DBLOG_P1)->first());
         });
 
         view()->composer('ads.includes.background-page',function (View $view){
-            $view->with('ad',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::DESKTOP_BACKGROUND_PAGE)->first());
+            $view->with('ad',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::GROUP_BACKGROUND_PAGE)->first());
         });
         view()->composer('ads.includes.adsdiretta',function (View $view){
-            $view->with('ad',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::DESKTOP_diretta_1)->first());
+            $view->with('ad',Ad::query()->typeAnnuncioImmagine()->whereGroup(Ad::GROUP_diretta_1)->first());
         });
     }
 }
