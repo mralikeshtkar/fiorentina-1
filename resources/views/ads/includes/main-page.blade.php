3416b2c6737dd19042555d4c3c93734432f23d7a
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-08-19 19:03:00 +0200

    Update main-page.blade.php

--- resources/views/ads/includes/main-page.blade.php
+++ resources/views/ads/includes/main-page.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row mx-0">
             @foreach($ads as $ad)
-                <div class="col-12 col-md-6">
+                <div class="col-12">
                     <a href="" class="d-block w-full">
                         <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="w-full d-block">
                     </a>
