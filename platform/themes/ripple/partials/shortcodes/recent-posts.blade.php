d81bd84b07b14b38895ca85577f3fd7c55416773
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-08-19 19:15:54 +0200

    Update recent-posts.blade.php

--- platform/themes/ripple/partials/shortcodes/recent-posts.blade.php
+++ platform/themes/ripple/partials/shortcodes/recent-posts.blade.php
@@ -1,4 +1,4 @@
-<section class="section recent-posts pt-50 pb-50"
+<section class="section recent-posts pt-20 pb-20"
     @if ($shortcode->background_color) style="background-color: {{ $shortcode->background_color }} !important;" @endif>
     <div class="container bg-white">
         <div class="row" >
