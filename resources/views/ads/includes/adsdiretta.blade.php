511718442800a702e6634bf7d8859b015b6bc14e
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-08-20 11:15:49 +0200

    Update adsdiretta.blade.php

--- resources/views/ads/includes/adsdiretta.blade.php
+++ resources/views/ads/includes/adsdiretta.blade.php
@@ -1,7 +1,7 @@
 @if(isset($ad) && $ad)
     <div class="row mx-0">
 
-        <div class="col-12 col-md-6 mb-4">
+        <div class="col-12 col-md-8 col-lg-6 mx-auto">
             <a href="" class="d-block">
                 <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid" style="width: 100%; height: auto;">
             </a>
