c15048e3240a6df6b350ec2b3977ef4b82806a36
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-08-20 18:37:05 +0200

    Update calendario.blade.php

--- resources/views/ads/includes/calendario.blade.php
+++ resources/views/ads/includes/calendario.blade.php
@@ -18,60 +18,61 @@
                         $updateScheduledMessage = App\Http\Controllers\StandingController::fetchScheduledMatches();
                     @endphp
                 </div>
-                <table class="table table-sm table-striped">
-                    <thead
-                        style="
-                            background: blueviolet;
-                            border: 1px solid white;
-                            color: white;
-                            font-weight: 900;
-                        ">
+                <table class="table table-striped">
+                    <thead>
                     <tr>
-                        <th style="border-right: 1px solid white;">Data</th>
-                        <th style="border-right: 1px solid white;">PT</th>
-                        <th style="border-right: 1px solid white;">G</th>
-                        <th style="border-right: 1px solid white;">V</th>
-                        <th style="border-right: 1px solid white;">N</th>
-                        <th style="border-right: 1px solid white;">P</th>
-                        <th>DR</th>
+                        <th>ID</th>
+                        <th>Match ID</th>
+                        <th>Venue</th>
+                        <th>Matchday</th>
+                        <th>Stage</th>
+                        <th>Group</th>
+                        <th>Match Date</th>
+                        <th>Status</th>
+                        <th>Home Team</th>
+                        <th>Away Team</th>
+                        <th>Score</th>
+                        <th>Odds</th>
+                        <th>Created At</th>
                     </tr>
                     </thead>
-                    <tbody
-                        style="
-                            background: white;
-                            border: 1px solid white;
-                        ">
-
+                    <tbody>
                     @foreach (App\Models\Matches::all() as $match)
-                        @dd($match)
-                        <tr style="border-bottom:1px solid blueviolet">
-                            <td>{{ $match->match_date}}</td>
-{{--                            <td @if ($standing->short_name == 'Fiorentina') style="background-color:#8a2be270 !important;" @endif--}}
-{{--                            style="border-right: 1px solid blueviolet;"><img--}}
-{{--                                    src="{{ $standing->crest_url }}" width="15">--}}
-{{--                                {{ $standing->short_name }}</td>--}}
-{{--                            <td @if ($standing->short_name == 'Fiorentina') style="background-color:#8a2be270 !important;text-align:center" @endif--}}
-{{--                            style="border-right: 1px solid blueviolet;">{{ $standing->points }}</td>--}}
-{{--                            <td @if ($standing->short_name == 'Fiorentina') style="background-color:#8a2be270 !important;text-align:center" @endif--}}
-{{--                            style="border-right: 1px solid blueviolet;text-align:center">--}}
-{{--                                {{ $standing->played_games }}--}}
-{{--                            </td>--}}
-{{--                            <td @if ($standing->short_name == 'Fiorentina') style="background-color:#8a2be270 !important;text-align:center" @endif--}}
-{{--                            style="border-right: 1px solid blueviolet;text-align:center">--}}
-{{--                                {{ $standing->won }}</td>--}}
-{{--                            <td @if ($standing->short_name == 'Fiorentina') style="background-color:#8a2be270 !important;text-align:center" @endif--}}
-{{--                            style="border-right: 1px solid blueviolet;text-align:center">--}}
-{{--                                {{ $standing->draw }}</td>--}}
-{{--                            <td @if ($standing->short_name == 'Fiorentina') style="background-color:#8a2be270 !important;text-align:center" @endif--}}
-{{--                            style="border-right: 1px solid blueviolet;text-align:center">--}}
-{{--                                {{ $standing->lost }}</td>--}}
-{{--                            <td @if ($standing->short_name == 'Fiorentina') style="background-color:#8a2be270 !important;text-align:center" @endif--}}
-{{--                            style="text-align:center">{{ $standing->goal_difference }}</td>--}}
-
+                        @php
+                            // Decode the JSON fields
+                            $homeTeam = json_decode($match->home_team, true);
+                            $awayTeam = json_decode($match->away_team, true);
+                            $score = json_decode($match->score, true);
+                            $odds = json_decode($match->odds, true);
+                        @endphp
+                        <tr>
+                            <td>{{ $match->id }}</td>
+                            <td>{{ $match->match_id }}</td>
+                            <td>{{ $match->venue ?? 'N/A' }}</td>
+                            <td>{{ $match->matchday }}</td>
+                            <td>{{ $match->stage }}</td>
+                            <td>{{ $match->group ?? 'N/A' }}</td>
+                            <td>{{ $match->match_date }}</td>
+                            <td>{{ $match->status }}</td>
+                            <td>
+                                {{ $homeTeam['name'] }} ({{ $homeTeam['shortName'] }})
+                                <img src="{{ $homeTeam['crest'] }}" alt="{{ $homeTeam['shortName'] }}" style="width: 30px; height: auto;">
+                            </td>
+                            <td>
+                                {{ $awayTeam['name'] }} ({{ $awayTeam['shortName'] }})
+                                <img src="{{ $awayTeam['crest'] }}" alt="{{ $awayTeam['shortName'] }}" style="width: 30px; height: auto;">
+                            </td>
+                            <td>
+                                Full Time: {{ $score['fullTime']['home'] ?? '-' }} - {{ $score['fullTime']['away'] ?? '-' }} <br>
+                                Half Time: {{ $score['halfTime']['home'] ?? '-' }} - {{ $score['halfTime']['away'] ?? '-' }}
+                            </td>
+                            <td>{{ $odds['msg'] ?? 'N/A' }}</td>
+                            <td>{{ $match->created_at }}</td>
                         </tr>
                     @endforeach
                     </tbody>
                 </table>
+
             </section>
         </div>
     </div>
