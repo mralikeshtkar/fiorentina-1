abd35632773ff22d65b1590cc45ea730df7d2a8b
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-08-26 11:11:39 +0200

    Update view.blade.php

--- resources/views/votes/view.blade.php
+++ resources/views/votes/view.blade.php
@@ -1,19 +1,19 @@
 @extends(BaseHelper::getAdminMasterLayoutTemplate())
+
 @section('content')
 
     <div class="w-100">
         <div class="mb-3">
-            <a href="{{ route('votes.create') }}" class="btn btn-primary">Crrytate</a>
+            <a href="{{ route('votes.create') }}" class="btn btn-primary">Create</a>
         </div>
         <table class="table table-striped">
             <thead>
             <tr>
                 <th>ID</th>
-                <th>Title</th>
-                <th>type</th>
-                <th>group</th>
-                <th>image</th>
-
+                <th>Player Name</th>
+                <th>Vote Number</th>
+                <th>Created At</th>
+                <th>Updated At</th>
                 <th>Actions</th>
             </tr>
             </thead>
@@ -21,21 +21,14 @@
             @foreach($votes as $vote)
                 <tr>
                     <td class="align-middle">{{ $vote->id }}</td>
-                    <td class="align-middle">{{ $vote->title }}</td>
-                    <td class="align-middle">{{ $vote->type }}</td>
-                    <td class="align-middle">{{ $vote->group_name }}</td>
-                    <td class="align-middle">
-                        @if($vote->getImageUrl())
-                            <img src="{{ $vote->getImageUrl() }}" width="140" alt="{{ $vote->title }}">
-                        @endif
-                    </td>
-
-
-
+                    <td class="align-middle">{{ $vote->player->name }}</td>
+                    <td class="align-middle">{{ $vote->vote_number }}</td>
+                    <td class="align-middle">{{ $vote->created_at }}</td>
+                    <td class="align-middle">{{ $vote->updated_at }}</td>
                     <td class="align-middle">
                         <div class="d-flex gap-2">
-                            <a href="{{ route('votes.edit',$vote->id) }}" class="btn btn-primary">Edit</a>
-                            <form action="{{ route('votes.destroy',$vote->id) }}" method="post">
+                            <a href="{{ route('votes.edit', $vote->id) }}" class="btn btn-primary">Edit</a>
+                            <form action="{{ route('votes.destroy', $vote->id) }}" method="post">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-danger">Delete</button>
