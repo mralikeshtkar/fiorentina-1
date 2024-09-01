55221b763fa31717976e24f20ad5bbcb7a911f57
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-01 12:24:34 +0200

    Update adsense.blade.php

--- resources/views/ads/includes/adsense.blade.php
+++ resources/views/ads/includes/adsense.blade.php
@@ -1,4 +1,4 @@
-<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6741446998584415" data-ad-slot="166632497"
+<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6741446998584415" data-ad-slot="173156857"
     data-ad-format="auto"></ins>
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
