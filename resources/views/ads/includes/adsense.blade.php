900ad5adf5c6e34612cc88cd877e5b5a29d59b66
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-09-01 12:00:33 +0200

    Update adsense.blade.php

--- resources/views/ads/includes/adsense.blade.php
+++ resources/views/ads/includes/adsense.blade.php
@@ -1,6 +1,6 @@
 <ins class="adsbygoogle"
      style="display:block"
-     data-ad-client="ca-pub-XXXXXXXXXXXXXXXX"
+     data-ad-client="ca-pub-6741446998584415"
      data-ad-slot="YYYYYYYYYY"
      data-ad-format="auto"></ins>
 <script>
