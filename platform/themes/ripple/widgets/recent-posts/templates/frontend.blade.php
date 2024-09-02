2d815390d2ccd758286f66285a28df475c2b9e2b
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-02 13:10:25 +0200

    Update frontend.blade.php

--- platform/themes/ripple/widgets/recent-posts/templates/frontend.blade.php
+++ platform/themes/ripple/widgets/recent-posts/templates/frontend.blade.php
@@ -80,3 +80,6 @@
         </div>
     </div>
 @endif
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
