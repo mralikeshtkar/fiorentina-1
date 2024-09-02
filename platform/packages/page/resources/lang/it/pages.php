5bb1420f3921e20ef1b8a80cadbc06658f90a66d
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-09-02 12:19:18 +0200

    l

---
+++ platform/packages/page/resources/lang/it/pages.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'create' => 'Crea nuova pagina',
+    'form' => [
+        'name' => 'Nome',
+        'name_placeholder' => 'Nome della pagina (Massimo 120 caratteri)',
+        'content' => 'Contenuto',
+        'note' => 'Nota contenuto',
+    ],
+    'notices' => [
+        'no_select' => 'Seleziona almeno un record per eseguire questa azione!',
+        'update_success_message' => 'Aggiornamento avvenuto con successo',
+    ],
+    'cannot_delete' => 'La pagina non può essere eliminata',
+    'deleted' => 'Pagina eliminata',
+    'pages' => 'Pagine',
+    'menu' => 'Pagine',
+    'menu_name' => 'Pagine',
+    'edit_this_page' => 'Modifica questa pagina',
+    'total_pages' => 'Totale pagine',
+    'front_page' => 'Pagina iniziale',
+    'theme_options' => [
+        'title' => 'Pagina',
+        'your_home_page_display' => 'La tua homepage visualizza',
+    ],
+];
