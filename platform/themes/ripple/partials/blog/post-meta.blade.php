b4a1f84b7e0f0693c1360cc8c5decaf17110cb1e
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-02 10:10:47 +0200

    Update post-meta.blade.php

--- platform/themes/ripple/partials/blog/post-meta.blade.php
+++ platform/themes/ripple/partials/blog/post-meta.blade.php
@@ -1,14 +1,13 @@
-
-
 <span class="created_at " style="color: gray;">
     {!! BaseHelper::renderIcon('ti ti-clock') !!} {{ Theme::formatDate($post->created_at) }}
 </span>
 
-{{--@if ($post->author->name)--}}
-{{--    <span class="post-author text-light">{!! BaseHelper::renderIcon('ti ti-user-circle') !!} <span--}}
-{{--            class="text-light">{{ $post->author->name }}</span></span>--}}
-{{--@endif--}}
+{{-- @if ($post->author->name) --}}
+{{--    <span class="post-author text-light">{!! BaseHelper::renderIcon('ti ti-user-circle') !!} <span --}}
+{{--            class="text-light">{{ $post->author->name }}</span></span> --}}
+{{-- @endif --}}
 @if ($post->author->name)
+    @dd($post->author)
     <span class="post-author " style="color: gray;">{!! BaseHelper::renderIcon('ti ti-user-circle') !!}
         <span style="color: blueviolet;">{{ $post->author->name }}</span>
     </span>
