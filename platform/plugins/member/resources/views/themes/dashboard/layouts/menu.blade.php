7da34bb7ce3bcfcc0672954ec8e51fabca839cd2
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-09-02 10:19:20 +0200

    Update menu.blade.php

--- platform/plugins/member/resources/views/themes/dashboard/layouts/menu.blade.php
+++ platform/plugins/member/resources/views/themes/dashboard/layouts/menu.blade.php
@@ -1,6 +1,6 @@
 {{--<ul class="menu">--}}
 {{--    @foreach (DashboardMenu::getAll('member') as $item)--}}
-{{--        @dd($item)--}}
+
 {{--        @continue(! $item['name'])--}}
 {{--        <li>--}}
 {{--            <a--}}
@@ -15,7 +15,7 @@
 {{--</ul>--}}
 {{--<ul class="menu " style="background-color: violet;">--}}
 {{--<ul class="menu " >--}}
-{{--    --}}
+
 {{--    @foreach (DashboardMenu::getAll('member') as $item)--}}
 {{--        @continue(! $item['name'] || $item['name'] === 'posts')--}}
 {{--        <li>--}}
@@ -29,12 +29,17 @@
 {{--        </li>--}}
 {{--    @endforeach--}}
 {{--</ul>--}}
+
 <ul class="menu">
     @foreach (DashboardMenu::getAll('member') as $item)
-            <?php dd($item); ?>
+        @continue(! $item['name']) <!-- This skips any items without a name -->
+
+        <!-- Display the item name -->
+        <li>{{ $item['name'] }}</li>
+
         <li>
             <a
-                href="{{ $item['url'] }}"
+                href="{{ $item['url']  }}"
                 @class(['active' => $item['active'] && $item['url'] !== BaseHelper::getHomepageUrl()])
             >
                 <x-core::icon :name="$item['icon']" />
