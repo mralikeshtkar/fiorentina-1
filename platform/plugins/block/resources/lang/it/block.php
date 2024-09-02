11c06d76e68057d0e8bc9d17a4557df601b342c3
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-09-02 12:35:40 +0200

    67

---
+++ platform/plugins/block/resources/lang/it/block.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => 'Blocco',
+    'create' => 'Nuovo blocco',
+    'menu' => 'Blocchi statici',
+    'static_block_short_code_name' => 'Blocco statico',
+    'static_block_short_code_description' => 'Aggiungi un blocco statico personalizzato',
+    'alias' => 'Alias',
+];
---
+++ platform/plugins/blog/resources/lang/it/base.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'menu_name' => 'Blog',
+    'blog_page' => 'Pagina del Blog',
+    'select' => '-- Seleziona --',
+    'blog_page_id' => 'Pagina del blog',
+    'number_posts_per_page' => 'Numero di post per pagina',
+    'write_some_tags' => 'Scrivi alcuni tag',
+    'short_code_name' => 'Post del blog',
+    'short_code_description' => 'Aggiungi post del blog',
+    'number_posts_per_page_in_category' => 'Numero di post per pagina in una categoria',
+    'number_posts_per_page_in_tag' => 'Numero di post per pagina in un tag',
+    'settings' => [
+        'title' => 'Blog',
+        'description' => 'Visualizza e aggiorna le impostazioni del blog',
+        'enable_blog_post_schema' => 'Abilitare lo schema del post del blog?',
+        'enable_blog_post_schema_description' => 'Per saperne di più: https://schema.org/Article',
+        'schema_type' => 'Tipo di schema',
+    ],
+];
