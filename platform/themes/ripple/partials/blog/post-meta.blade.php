6492a607c3badf338161a16ed872d241df1dbf37
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-02 10:27:53 +0200

    Update post-meta.blade.php

--- platform/themes/ripple/partials/blog/post-meta.blade.php
+++ platform/themes/ripple/partials/blog/post-meta.blade.php
@@ -4,21 +4,27 @@
     $formattedDate = $date->format('j F Y - H:i');
 
 @endphp
-<span class="created_at " style="color: gray;">
-    {!! BaseHelper::renderIcon('ti ti-clock') !!} {{ $formattedDate }}
-</span>
+
 
 
 @if ($post->author->name)
+    <div class="row">
+        <div class="col-lg-6">
+            <span class="created_at " style="color: gray;">
+                {!! BaseHelper::renderIcon('ti ti-clock') !!} {{ $formattedDate }}
+            </span>
+            @if ($post->author->avatar->url)
+                <img class="post-author" src="{{ $post->author->avatar->url }}" alt="$post->author->avatar->url">
+            @else
+                <span class="post-author " style="color: gray;">{!! BaseHelper::renderIcon('ti ti-user-circle') !!}
+            @endif
+        </div>
+        <div class="col-6">a</div>
+    </div>
 
-    @if ($post->author->avatar->url)
-        <img class="post-author" src="{{ $post->author->avatar->url }}" alt="$post->author->avatar->url">
-    @else
-        <span class="post-author " style="color: gray;">{!! BaseHelper::renderIcon('ti ti-user-circle') !!}
-    @endif
 
     <span class="author-name">{{ $post->author->name }}</span>
-    </span>
+
 @endif
 
 <div class="row">
