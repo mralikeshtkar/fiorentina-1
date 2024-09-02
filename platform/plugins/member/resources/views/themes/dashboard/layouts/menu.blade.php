d9907092c5b08e8d7699bcd01c431efd0f9db261
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-09-02 09:33:07 +0200

    Update menu.blade.php

--- platform/plugins/member/resources/views/themes/dashboard/layouts/menu.blade.php
+++ platform/plugins/member/resources/views/themes/dashboard/layouts/menu.blade.php
@@ -1,23 +1,7 @@
-{{--<ul class="menu">--}}
-{{--    @foreach (DashboardMenu::getAll('member') as $item)--}}
-{{--        @continue(! $item['name'])--}}
-{{--        <li>--}}
-{{--            <a--}}
-{{--                href="{{ $item['url']  }}"--}}
-{{--                @class(['active' => $item['active'] && $item['url'] !== BaseHelper::getHomepageUrl()])--}}
-{{--            >--}}
-{{--                <x-core::icon :name="$item['icon']" />--}}
-{{--                {{ __($item['name']) }}--}}
-{{--            </a>--}}
-{{--        </li>--}}
-{{--    @endforeach--}}
-{{--</ul>--}}
 <ul class="menu">
-
     @foreach (DashboardMenu::getAll('member') as $item)
         @dd($item)
-        @continue(! $item['name'] || $item['name'] === 'posts')
-
+        @continue(! $item['name'])
         <li>
             <a
                 href="{{ $item['url']  }}"
@@ -29,4 +13,21 @@
         </li>
     @endforeach
 </ul>
+{{--<ul class="menu">--}}
+
+{{--    @foreach (DashboardMenu::getAll('member') as $item)--}}
+{{--        @dd($item)--}}
+{{--        @continue(! $item['name'] || $item['name'] === 'posts')--}}
+
+{{--        <li>--}}
+{{--            <a--}}
+{{--                href="{{ $item['url']  }}"--}}
+{{--                @class(['active' => $item['active'] && $item['url'] !== BaseHelper::getHomepageUrl()])--}}
+{{--            >--}}
+{{--                <x-core::icon :name="$item['icon']" />--}}
+{{--                {{ __($item['name']) }}--}}
+{{--            </a>--}}
+{{--        </li>--}}
+{{--    @endforeach--}}
+{{--</ul>--}}
 
