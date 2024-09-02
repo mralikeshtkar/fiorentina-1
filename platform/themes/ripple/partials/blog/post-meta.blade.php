ffbdcc1c02208610a26a78a26578d5b115c7a910
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-02 10:11:19 +0200

    Update post-meta.blade.php

--- platform/themes/ripple/partials/blog/post-meta.blade.php
+++ platform/themes/ripple/partials/blog/post-meta.blade.php
@@ -7,7 +7,7 @@
 {{--            class="text-light">{{ $post->author->name }}</span></span> --}}
 {{-- @endif --}}
 @if ($post->author->name)
-    @dd($post->author)
+    @dd($post->author->avatar)
     <span class="post-author " style="color: gray;">{!! BaseHelper::renderIcon('ti ti-user-circle') !!}
         <span style="color: blueviolet;">{{ $post->author->name }}</span>
     </span>
