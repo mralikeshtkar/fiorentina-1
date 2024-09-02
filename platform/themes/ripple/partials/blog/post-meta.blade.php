38d911660bc1c88335f7a0248b12e1ffb6c700e8
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-02 10:33:38 +0200

    Update post-meta.blade.php

--- platform/themes/ripple/partials/blog/post-meta.blade.php
+++ platform/themes/ripple/partials/blog/post-meta.blade.php
@@ -21,7 +21,16 @@
             <span class="author-name">{{ $post->author->name }}</span>
 
         </div>
-        <div class="col-6">a</div>
+        <div class="col-6">
+            <div class="social-buttons">
+                <a href="#" class="social-btn facebook"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" class="social-btn twitter"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="social-btn pinterest"><i class="fab fa-pinterest-p"></i></a>
+                <a href="#" class="social-btn email"><i class="fas fa-envelope"></i></a>
+                <a href="#" class="social-btn comment"><i class="fas fa-comment"></i></a>
+            </div>
+
+        </div>
     </div>
 
 
