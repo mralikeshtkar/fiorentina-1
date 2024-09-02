b60fe24c1c950f2a8d6a126993bd1a7e70fc19e7
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-09-02 09:49:44 +0200

    Update menu.blade.php

--- platform/plugins/member/resources/views/themes/dashboard/layouts/menu.blade.php
+++ platform/plugins/member/resources/views/themes/dashboard/layouts/menu.blade.php
@@ -14,29 +14,12 @@
 {{--    @endforeach--}}
 {{--</ul>--}}
 {{--<ul class="menu " style="background-color: violet;">--}}
-{{--<ul class="menu " >--}}
-{{--    @foreach (DashboardMenu::getAll('member') as $item)--}}
-{{--        @continue(! $item['name'] || $item['name'] === 'posts')--}}
-{{--        <li>--}}
-{{--            <a--}}
-{{--                href="{{ $item['url']  }}"--}}
-{{--                @class(['active' => $item['active'] && $item['url'] !== BaseHelper::getHomepageUrl()])--}}
-{{--            >--}}
-{{--                <x-core::icon :name="$item['icon']" />--}}
-{{--                {{ __($item['name']) }}--}}
-{{--            </a>--}}
-{{--        </li>--}}
-{{--    @endforeach--}}
-{{--</ul>--}}
-<ul class="menu">
+<ul class="menu " >
     @foreach (DashboardMenu::getAll('member') as $item)
         @continue(! $item['name'] || $item['name'] === 'posts')
-
-        @dd($item) <!-- This will dump the contents of the $item and stop execution -->
-
         <li>
             <a
-                href="{{ $item['url'] }}"
+                href="{{ $item['url']  }}"
                 @class(['active' => $item['active'] && $item['url'] !== BaseHelper::getHomepageUrl()])
             >
                 <x-core::icon :name="$item['icon']" />
@@ -45,6 +28,7 @@
         </li>
     @endforeach
 </ul>
+
 
 
 
