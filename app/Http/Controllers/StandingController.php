9ae8f886ea8c9ca26634ac0df299f7860adb73c9
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-07 13:50:41 +0200

    Update StandingController.php

--- app/Http/Controllers/StandingController.php
+++ app/Http/Controllers/StandingController.php
@@ -180,7 +180,7 @@ class StandingController extends Controller
         // Step 4: Find specific elements, for example, a <script> tag with embedded JSON data
         $scriptTags = $xpath->query('//script');
 
-        dd($scriptTags);
+        dd($xpath);
 
         // Initialize an array to store matches
         $matches = [];
