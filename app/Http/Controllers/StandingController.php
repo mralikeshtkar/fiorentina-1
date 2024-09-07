55a2c8949b094d3cb1bbf30812e4ed598d124499
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-07 15:11:29 +0200

    Update StandingController.php

--- app/Http/Controllers/StandingController.php
+++ app/Http/Controllers/StandingController.php
@@ -172,7 +172,6 @@ class StandingController extends Controller
         // Query all elements with the class 'event--fixtures'
         $fixtures = $xpath->query("//div[contains(@class, 'event--fixtures')]");
 
-        dd($fixtures);
     
         // Create a new DOMDocument to store only the desired content
         $newDom = new \DOMDocument();
