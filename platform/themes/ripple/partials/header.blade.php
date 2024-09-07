aac5e35c2038d3d8217c5c22dc9ec88e3ce85c27
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-09-07 18:45:27 +0200

    Update header.blade.php

--- platform/themes/ripple/partials/header.blade.php
+++ platform/themes/ripple/partials/header.blade.php
@@ -85,8 +85,9 @@
             <div class="page-header__right flex-grow-1">
                 <div class="navigation-toggle " style="display: none"><span></span></div>
                 <div class="float-start w-100" style="display: flex; align-items: center; justify-content: space-between;">
-                    <nav class="navigation navigation--light navigation--fade navigation--fadeLeft" style="display: flex; align-items: center; flex-grow: 1;">
-                        {!! Menu::renderMenuLocation('main-menu', [
+                    <nav class="navigation navigation--light navigation--fade navigation--fadeLeft" style="display: flex; align-items: center; flex-grow: 1; background-color: violet;">
+
+                    {!! Menu::renderMenuLocation('main-menu', [
                             'options' => ['class' => 'menu sub-menu--slideLeft', 'style' => 'display: flex; align-items: center; margin: 0; padding: 0; list-style: none;'],
                             'view' => 'main-menu',
                         ]) !!}
