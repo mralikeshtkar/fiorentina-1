f77de9f8fb2838eea95ceb9adb831761a28dc48d
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-09 00:05:07 +0200

    Update diretta.blade.php

--- resources/views/ads/includes/diretta.blade.php
+++ resources/views/ads/includes/diretta.blade.php
@@ -8,6 +8,7 @@
         $match = Matches::where('match_id', $matchId)->first();
         MatchLineupsController::storeLineups($matchId);
         // Filter the lineups by formation_name
+        $lineups = MatchLineups::where('match_id', $matchId)->get();
         $filteredLineups = $lineups->filter(function ($lineup) {
             return in_array($lineup->formation_name, ['Panchina', 'Allenatori', 'Formazioni iniziali']);
         });
