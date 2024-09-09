0d126c119260a2efb3c8e3aa8fe4171eb3268fa5
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-09 11:30:28 +0200

    1130

--- platform/themes/ripple/partials/header.blade.php
+++ platform/themes/ripple/partials/header.blade.php
@@ -17,7 +17,7 @@
 <body {!! Theme::bodyAttributes() !!}>
     {!! apply_filters(THEME_FRONT_BODY, null) !!}
     <header data-sticky="false" data-sticky-checkpoint="200" data-responsive="991"
-            class="page-header page-header--light py-0">
+        class="page-header page-header--light py-0">
         <div class="container headup" style="display: flex; justify-content: space-between; align-items: center;">
             <div class="page-header__left">
                 <a href="{{ BaseHelper::getHomepageUrl() }}" class="page-logo">
@@ -26,7 +26,8 @@
             </div>
             <div class="page-header__right" style="display: flex; align-items: center;">
                 @if ($socialLinks = Theme::getSocialLinks())
-                    <ul class="social social--simple" style="display: flex; margin-right: 15px; list-style: none; padding: 0;">
+                    <ul class="social social--simple"
+                        style="display: flex; margin-right: 15px; list-style: none; padding: 0;">
                         @foreach ($socialLinks as $socialLink)
                             @continue(!($icon = $socialLink->getIconHtml()))
 
@@ -35,7 +36,6 @@
                                     {{ $icon }}
                                 </a>
                             </li>
-
                         @endforeach
                     </ul>
                 @endif
@@ -47,50 +47,51 @@
         </div>
     </header>
 
-{{--    <header class="header" id="header">--}}
-{{--        <div class="header-wrap d-none d-sm-block h-34px">--}}
+    {{--    <header class="header" id="header"> --}}
+    {{--        <div class="header-wrap d-none d-sm-block h-34px"> --}}
 
-{{--            <nav class="nav-top">--}}
-{{--                <div class="container">--}}
-{{--                    <div class="row">--}}
+    {{--            <nav class="nav-top"> --}}
+    {{--                <div class="container"> --}}
+    {{--                    <div class="row"> --}}
 
-{{--                        @if ($socialLinks = Theme::getSocialLinks())--}}
+    {{--                        @if ($socialLinks = Theme::getSocialLinks()) --}}
 
 
-{{--                            <div class="col-sm-4 d-flex align-items-center h-34px">--}}
-{{--                                <ul class="social social--simple">--}}
-{{--                                    @foreach ($socialLinks as $socialLink)--}}
-{{--                                        @continue(!($icon = $socialLink->getIconHtml()))--}}
+    {{--                            <div class="col-sm-4 d-flex align-items-center h-34px"> --}}
+    {{--                                <ul class="social social--simple"> --}}
+    {{--                                    @foreach ($socialLinks as $socialLink) --}}
+    {{--                                        @continue(!($icon = $socialLink->getIconHtml())) --}}
 
-{{--                                        <li>--}}
-{{--                                            <a {{ $socialLink->getAttributes() }}>--}}
-{{--                                                {{ $icon }}--}}
-{{--                                            </a>--}}
-{{--                                        </li>--}}
+    {{--                                        <li> --}}
+    {{--                                            <a {{ $socialLink->getAttributes() }}> --}}
+    {{--                                                {{ $icon }} --}}
+    {{--                                            </a> --}}
+    {{--                                        </li> --}}
 
-{{--                                    @endforeach--}}
-{{--                                </ul>--}}
-{{--                            </div>--}}
-{{--                        @endif--}}
+    {{--                                    @endforeach --}}
+    {{--                                </ul> --}}
+    {{--                            </div> --}}
+    {{--                        @endif --}}
 
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </nav>--}}
-{{--        </div>--}}
-{{--    </header>--}}
+    {{--                    </div> --}}
+    {{--                </div> --}}
+    {{--            </nav> --}}
+    {{--        </div> --}}
+    {{--    </header> --}}
     <header data-sticky="false" data-sticky-checkpoint="200" data-responsive="991"
-            class="page-header page-header--light py-0">
+        class="page-header page-header--light py-0">
         <div class="container d-flex">
 
             <div class="page-header__right flex-grow-1">
                 <div class="navigation-toggle " style="display: none"><span></span></div>
-                <div class="float-start w-100" style="display: flex; align-items: center; justify-content: space-between;">
-{{--                    <nav class="navigation navigation--light navigation--fadeRight" style="display: flex; align-items: center; flex-grow: 1; ">--}}
-                    <nav class="navigation navigation--light navigation--fadeRight"  >
+                <div class="float-start w-100"
+                    style="display: flex; align-items: center; justify-content: space-between;">
+                    {{--                    <nav class="navigation navigation--light navigation--fadeRight" style="display: flex; align-items: center; flex-grow: 1; "> --}}
+                    <nav class="navigation navigation--light navigation--fadeRight">
 
 
-                    {!! Menu::renderMenuLocation('main-menu', [
-                            'options' => ['class' => 'menu sub-menu--slideLeft', 'style' => 'display: flex; align-items: center; margin: 0; padding: 0; list-style: none;'],
+                        {!! Menu::renderMenuLocation('main-menu', [
+                            'options' => ['class' => 'menu sub-menu--slideLeft'],
                             'view' => 'main-menu',
                         ]) !!}
 
@@ -101,19 +102,24 @@
                             @if (is_plugin_active('member'))
                                 @if (auth('member')->check())
                                     <li class=" d-block d-sm-none" style="margin-left: 20px;">
-                                        <a href="{{ route('public.member.dashboard') }}" rel="nofollow" style="display: flex; align-items: center;">
-                                            <img src="{{ auth('member')->user()->avatar_thumb_url }}" class="img-circle" width="20" alt="{{ auth('member')->user()->name }}" loading="lazy">
+                                        <a href="{{ route('public.member.dashboard') }}" rel="nofollow"
+                                            style="display: flex; align-items: center;">
+                                            <img src="{{ auth('member')->user()->avatar_thumb_url }}" class="img-circle"
+                                                width="20" alt="{{ auth('member')->user()->name }}" loading="lazy">
                                             &nbsp;<span>{{ auth('member')->user()->name }}</span>
                                         </a>
                                     </li>
                                     <li class=" d-block d-sm-none" style="margin-left: 20px;">
-                                        <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" rel="nofollow" style="display: flex; align-items: center;">
+                                        <a href="#"
+                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                                            rel="nofollow" style="display: flex; align-items: center;">
                                             {!! BaseHelper::renderIcon('ti ti-login-2') !!} {{ __('Logout') }}
                                         </a>
                                     </li>
                                 @else
                                     <li class=" d-block d-sm-none" style="margin-left: 20px;">
-                                        <a href="{{ route('public.member.login') }}" rel="nofollow" style="display: flex; align-items: center;">
+                                        <a href="{{ route('public.member.login') }}" rel="nofollow"
+                                            style="display: flex; align-items: center;">
                                             {!! BaseHelper::renderIcon('ti ti-login-2') !!} {{ __('Login') }}
                                         </a>
                                     </li>
@@ -123,56 +129,57 @@
                     </nav>
 
                     @if (auth('member')->check())
-                        <form id="logout-form" action="{{ route('public.member.logout') }}" method="POST" style="display: none;">
+                        <form id="logout-form" action="{{ route('public.member.logout') }}" method="POST"
+                            style="display: none;">
                             @csrf
                         </form>
                     @endif
                 </div>
 
-                {{--                <div class="float-start w-100">--}}
+                {{--                <div class="float-start w-100"> --}}
 
-{{--                    <nav class="navigation navigation--light navigation--fade navigation--fadeLeft">--}}
-{{--                        {!! Menu::renderMenuLocation('main-menu', [--}}
-{{--                            'options' => ['class' => 'menu sub-menu--slideLeft'],--}}
-{{--                            'view' => 'main-menu',--}}
-{{--                        ]) !!}--}}
-
-
+                {{--                    <nav class="navigation navigation--light navigation--fade navigation--fadeLeft"> --}}
+                {{--                        {!! Menu::renderMenuLocation('main-menu', [ --}}
+                {{--                            'options' => ['class' => 'menu sub-menu--slideLeft'], --}}
+                {{--                            'view' => 'main-menu', --}}
+                {{--                        ]) !!} --}}
 
 
 
 
 
-{{--                        <li class="language-wrapper d-block d-sm-none">--}}
-{{--                            {!! apply_filters('language_switcher') !!}--}}
-{{--                        </li>--}}
-{{--                        @if (is_plugin_active('member'))--}}
-{{--                            <ul class="d-flex">--}}
-{{--                                @if (auth('member')->check())--}}
-{{--                                    <li><a href="{{ route('public.member.dashboard') }}" rel="nofollow"><img--}}
-{{--                                                src="{{ auth('member')->user()->avatar_thumb_url }}"--}}
-{{--                                                class="img-circle" width="20"--}}
-{{--                                                alt="{{ auth('member')->user()->name }}" loading="lazy">--}}
-{{--                                            &nbsp;<span>{{ auth('member')->user()->name }}</span></a></li>--}}
-{{--                                    <li><a href="#"--}}
-{{--                                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();"--}}
-{{--                                           rel="nofollow">{!! BaseHelper::renderIcon('ti ti-login-2') !!} {{ __('Logout') }}</a></li>--}}
-{{--                                @else--}}
-{{--                                    <li><a href="{{ route('public.member.login') }}"--}}
-{{--                                           rel="nofollow">{!! BaseHelper::renderIcon('ti ti-login-2') !!} {{ __('Login') }}</a></li>--}}
-{{--                                @endif--}}
-{{--                            </ul>--}}
-{{--                            @if (auth('member')->check())--}}
-{{--                                <form id="logout-form" action="{{ route('public.member.logout') }}" method="POST"--}}
-{{--                                      style="display: none;">--}}
-{{--                                    @csrf--}}
-{{--                                </form>--}}
-{{--                            @endif--}}
-{{--                        @endif--}}
 
-{{--                    </nav>--}}
 
-{{--                </div>--}}
+                {{--                        <li class="language-wrapper d-block d-sm-none"> --}}
+                {{--                            {!! apply_filters('language_switcher') !!} --}}
+                {{--                        </li> --}}
+                {{--                        @if (is_plugin_active('member')) --}}
+                {{--                            <ul class="d-flex"> --}}
+                {{--                                @if (auth('member')->check()) --}}
+                {{--                                    <li><a href="{{ route('public.member.dashboard') }}" rel="nofollow"><img --}}
+                {{--                                                src="{{ auth('member')->user()->avatar_thumb_url }}" --}}
+                {{--                                                class="img-circle" width="20" --}}
+                {{--                                                alt="{{ auth('member')->user()->name }}" loading="lazy"> --}}
+                {{--                                            &nbsp;<span>{{ auth('member')->user()->name }}</span></a></li> --}}
+                {{--                                    <li><a href="#" --}}
+                {{--                                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();" --}}
+                {{--                                           rel="nofollow">{!! BaseHelper::renderIcon('ti ti-login-2') !!} {{ __('Logout') }}</a></li> --}}
+                {{--                                @else --}}
+                {{--                                    <li><a href="{{ route('public.member.login') }}" --}}
+                {{--                                           rel="nofollow">{!! BaseHelper::renderIcon('ti ti-login-2') !!} {{ __('Login') }}</a></li> --}}
+                {{--                                @endif --}}
+                {{--                            </ul> --}}
+                {{--                            @if (auth('member')->check()) --}}
+                {{--                                <form id="logout-form" action="{{ route('public.member.logout') }}" method="POST" --}}
+                {{--                                      style="display: none;"> --}}
+                {{--                                    @csrf --}}
+                {{--                                </form> --}}
+                {{--                            @endif --}}
+                {{--                        @endif --}}
+
+                {{--                    </nav> --}}
+
+                {{--                </div> --}}
                 <div class="clearfix"></div>
             </div>
             <div class="clearfix"></div>
@@ -181,11 +188,10 @@
             <div class="super-search hide" data-search-url="{{ route('public.ajax.search') }}">
                 <form class="quick-search" action="{{ route('public.search') }}">
                     <input type="text" name="q" placeholder="{{ __('Type to search...') }}"
-                           class="form-control search-input" autocomplete="off">
+                        class="form-control search-input" autocomplete="off">
                     <span class="close-search">&times;</span>
                 </form>
                 <div class="search-result"></div>
             </div>
         @endif
     </header>
-
