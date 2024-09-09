22bf06acf79d0e844400c6bbb100bae8c8932845
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-09 09:27:52 +0200

    Update diretta.blade.php

--- resources/views/ads/includes/diretta.blade.php
+++ resources/views/ads/includes/diretta.blade.php
@@ -1,11 +1,11 @@
 {{-- Live One here  --}}
 @php
-    use App\Models\Matches;
+    use App\Models\Calendario;
     use App\Models\MatchLineups;
     use App\Http\Controllers\MatchLineupsController;
     $matchId = request()->query('match_id');
     if ($matchId) {
-        $match = Matches::where('match_id', $matchId)->first();
+        $match = Calendario::where('match_id', $matchId)->first();
         MatchLineupsController::storeLineups($matchId);
         // Filter the lineups by formation_name
         $lineups = MatchLineups::where('match_id', $matchId)->get();
