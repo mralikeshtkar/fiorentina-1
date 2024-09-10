aa39ddfe729a5e419b3be552919a456b23a0b0bb
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-10 09:47:59 +0200

    Update statistiche.blade.php

--- resources/views/ads/includes/statistiche.blade.php
+++ resources/views/ads/includes/statistiche.blade.php
@@ -3,14 +3,18 @@
 
     @foreach ($statics as $stat)
         @if ($stat['stage_name'] == 'Partita')
-            <div class="stat-row mb-4">
+            <div class="stat-row">
                 <!-- Home Value -->
                 <div class="stat-value">{{ $stat['value_home'] }}</div>
 
                 <!-- Stat Bar -->
                 <div class="stat-bar">
                     @php
-                        $maxValue = $stat['value_home'] + $stat['value_away'];
+                        if ($stat['incident_name'] == 'Possesso Palla') {
+                            $maxValue = 100;
+                        } else {
+                            $maxValue = $stat['value_home'] + $stat['value_away'];
+                        }
                         $homeWidth =
                             is_numeric($stat['value_home']) && is_numeric($stat['value_away'])
                                 ? ($stat['value_home'] / $maxValue) * 100
