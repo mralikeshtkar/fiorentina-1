c17743b813111403852b7adcfca93dff73c67c59
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-10 09:29:27 +0200

    Update MatchStaticsController.php

--- app/Http/Controllers/MatchStaticsController.php
+++ app/Http/Controllers/MatchStaticsController.php
@@ -10,38 +10,43 @@ class MatchStaticsController extends Controller
 {
     public static function storeMatchStatistics($matchId)
     {
-        // Simulate fetching data from an API, replace with your actual API request logic
-        $url="https://flashlive-sports.p.rapidapi.com/v1/events/statistics?event_id={$matchId}&locale=it_IT";
-        $response = Http::withHeaders([
-            "x-rapidapi-host" => 'flashlive-sports.p.rapidapi.com',
-            "x-rapidapi-key" => '1e9b76550emshc710802be81e3fcp1a0226jsn069e6c35a2bb'
-        ])->get($url);        
-        // Assuming the response returns the data in the format provided
-        $data = $response->json()['DATA'];
+        $match=MatchStatics::where('match_id',$matchId)->first();
+        if(!$match){
+            // Simulate fetching data from an API, replace with your actual API request logic
+            $url="https://flashlive-sports.p.rapidapi.com/v1/events/statistics?event_id={$matchId}&locale=it_IT";
+            $response = Http::withHeaders([
+                "x-rapidapi-host" => 'flashlive-sports.p.rapidapi.com',
+                "x-rapidapi-key" => '1e9b76550emshc710802be81e3fcp1a0226jsn069e6c35a2bb'
+            ])->get($url);        
+            // Assuming the response returns the data in the format provided
+            $data = $response->json()['DATA'];
 
-        // Loop through each stage and save the statistics
-        foreach ($data as $stage) {
-            $stageName = $stage['STAGE_NAME'];
+            // Loop through each stage and save the statistics
+            foreach ($data as $stage) {
+                $stageName = $stage['STAGE_NAME'];
 
-            foreach ($stage['GROUPS'] as $group) {
-                $groupLabel = $group['GROUP_LABEL'];
+                foreach ($stage['GROUPS'] as $group) {
+                    $groupLabel = $group['GROUP_LABEL'];
 
-                foreach ($group['ITEMS'] as $item) {
-                    MatchStatistics::updateOrCreate(
-                        [
-                            'match_id' => $matchId,
-                            'stage_name' => $stageName,
-                            'group_label' => $groupLabel,
-                            'incident_name' => $item['INCIDENT_NAME']
-                        ],
-                        [
-                            'value_home' => $item['VALUE_HOME'],
-                            'value_away' => $item['VALUE_AWAY']
-                        ]
-                    );
+                    foreach ($group['ITEMS'] as $item) {
+                        MatchStatistics::updateOrCreate(
+                            [
+                                'match_id' => $matchId,
+                                'stage_name' => $stageName,
+                                'group_label' => $groupLabel,
+                                'incident_name' => $item['INCIDENT_NAME']
+                            ],
+                            [
+                                'value_home' => $item['VALUE_HOME'],
+                                'value_away' => $item['VALUE_AWAY']
+                            ]
+                        );
+                    }
                 }
             }
+
         }
+        
 
         return response()->json(['success' => 'Match statistics saved successfully!']);
     }
