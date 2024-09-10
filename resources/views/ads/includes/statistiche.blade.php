cda487d3a22283773d6645114fdddc2222f45b43
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-10 09:36:47 +0200

    Update statistiche.blade.php

--- resources/views/ads/includes/statistiche.blade.php
+++ resources/views/ads/includes/statistiche.blade.php
@@ -2,45 +2,47 @@
 
 
     @foreach ($statics as $stat)
-        <div class="stat-row">
-            <!-- Home Value -->
-            <div class="stat-value">{{ $stat['value_home'] }}</div>
+        @if ($stat['stage_name'] == 'Partita')
+            <div class="stat-row">
+                <!-- Home Value -->
+                <div class="stat-value">{{ $stat['value_home'] }}</div>
 
-            <!-- Stat Bar -->
-            <div class="stat-bar">
-                @php
-                    // Calculate the width for home and away bars
-                    $maxValue = max($stat['value_home'], $stat['value_away']);
-                    $homeWidth =
-                        is_numeric($stat['value_home']) && is_numeric($stat['value_away'])
-                            ? ($stat['value_home'] / $maxValue) * 100
-                            : 0;
-                    $awayWidth =
-                        is_numeric($stat['value_home']) && is_numeric($stat['value_away'])
-                            ? ($stat['away'] / $maxValue) * 100
-                            : 0;
-                @endphp
-                <div class="stat-bar-fill 
+                <!-- Stat Bar -->
+                <div class="stat-bar">
+                    @php
+                        // Calculate the width for home and away bars
+                        $maxValue = max($stat['value_home'], $stat['value_away']);
+                        $homeWidth =
+                            is_numeric($stat['value_home']) && is_numeric($stat['value_away'])
+                                ? ($stat['value_home'] / $maxValue) * 100
+                                : 0;
+                        $awayWidth =
+                            is_numeric($stat['value_home']) && is_numeric($stat['value_away'])
+                                ? ($stat['away'] / $maxValue) * 100
+                                : 0;
+                    @endphp
+                    <div class="stat-bar-fill 
                 @if ($isHomeFiorentina) fiorentina-fill
                 @else
                 away-fill @endif
 
                 "
-                    style="width: {{ $homeWidth }}%;"></div>
-                <div class="stat-bar-fill 
+                        style="width: {{ $homeWidth }}%;"></div>
+                    <div class="stat-bar-fill 
                 @if (!$isHomeFiorentina) fiorentina-fill
                 @else
                 away-fill @endif
                 
                 "
-                    style="width: {{ $awayWidth }}%;"></div>
+                        style="width: {{ $awayWidth }}%;"></div>
+                </div>
+
+                <!-- Stat Label -->
+                <div class="stat-label text-dark">{{ $stat['incident_name'] }}</div>
+
+                <!-- Away Value -->
+                <div class="stat-value">{{ $stat['value_away'] }}</div>
             </div>
-
-            <!-- Stat Label -->
-            <div class="stat-label text-dark">{{ $stat['incident_name'] }}</div>
-
-            <!-- Away Value -->
-            <div class="stat-value">{{ $stat['value_away'] }}</div>
-        </div>
+        @endif
     @endforeach
 </div>
