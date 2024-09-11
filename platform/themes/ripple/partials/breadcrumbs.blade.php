e74467d6c323be5267edd862281fcfefe7258a29
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-11 13:23:10 +0200

    1323

--- platform/themes/ripple/partials/breadcrumbs.blade.php
+++ platform/themes/ripple/partials/breadcrumbs.blade.php
@@ -1,5 +1,5 @@
 <section
-    data-background="{{ Theme::get('breadcrumbBannerImage') ?: (theme_option('default_breadcrumb_banner_image') ? RvMedia::getImageUrl(theme_option('default_breadcrumb_banner_image')) : Theme::asset()->url('images/1275061.png')) }}"
+    data-background="{{ Theme::get('breadcrumbBannerImage') ?: (theme_option('default_breadcrumb_banner_image') ? RvMedia::getImageUrl(theme_option('default_breadcrumb_banner_image')) : Theme::asset()->url('images/1275062.png')) }}"
     class="section page-intro pt-50 pb-50 bg-cover">
     <div style="opacity: 0.6" class="bg-overlay"></div>
     <div class="container">
