c875adc5d475bb074347c01a004106e4ce70471e
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-11 11:55:30 +0200

    Update riassunto.blade.php

--- resources/views/ads/includes/riassunto.blade.php
+++ resources/views/ads/includes/riassunto.blade.php
@@ -1,44 +1,43 @@
+<div class="container">
+    @foreach ($summaries->groupBy('stage_name') as $stageName => $items)
+        <div class="stage">
+            <h4>{{ $stageName }}
+                @php
+                    $firstIncident = $items->first();
+                    $participants = json_decode($firstIncident->incident_participants, true);
 
-    <div class="container">
-        @foreach($matchSummary->groupBy('stage_name') as $stageName => $items)
-            <div class="stage">
-                <h4>{{ $stageName }}
-                    @php
-                        $firstIncident = $items->first();
-                        $participants = json_decode($firstIncident->incident_participants, true);
-
-                    @endphp
-                </h4>
-                @foreach($items as $item)
-                    <div class="incident" style="display: flex; align-items: center; margin-bottom: 10px;">
-                        <div class="incident-time" style="width: 50px;">
-                            {{ $item->incident_time }}
-                        </div>
-                        <div class="incident-detail" style="flex: 1;">
-                            <span class="{{ $item->incident_type }}">
-                                {{ ucfirst(str_replace('_', ' ', $item->incident_type)) }}:
-
-                                @php
-                                    $participants = json_decode($item->incident_participants, true);
-                                @endphp
-
-                                @if ($participants && is_array($participants))
-                                    @foreach($participants as $participant)
-                                        <strong>{{ $participant['participant_name'] }}</strong>
-                                        @if (!$loop->last)
-                                            , 
-                                        @endif
-                                    @endforeach
-                                @endif
-
-                                @if ($item->incident_type === 'GOAL' && isset($participants[0]['home_score']) && isset($participants[0]['away_score']))
-                                    <span>({{ $participants[0]['home_score'] }} - {{ $participants[0]['away_score'] }})</span>
-                                @endif
-                            </span>
-                        </div>
+                @endphp
+            </h4>
+            @foreach ($items as $item)
+                <div class="incident" style="display: flex; align-items: center; margin-bottom: 10px;">
+                    <div class="incident-time" style="width: 50px;">
+                        {{ $item->incident_time }}
                     </div>
-                @endforeach
-            </div>
-        @endforeach
-    </div>
+                    <div class="incident-detail" style="flex: 1;">
+                        <span class="{{ $item->incident_type }}">
+                            {{ ucfirst(str_replace('_', ' ', $item->incident_type)) }}:
 
+                            @php
+                                $participants = json_decode($item->incident_participants, true);
+                            @endphp
+
+                            @if ($participants && is_array($participants))
+                                @foreach ($participants as $participant)
+                                    <strong>{{ $participant['participant_name'] }}</strong>
+                                    @if (!$loop->last)
+                                        ,
+                                    @endif
+                                @endforeach
+                            @endif
+
+                            @if ($item->incident_type === 'GOAL' && isset($participants[0]['home_score']) && isset($participants[0]['away_score']))
+                                <span>({{ $participants[0]['home_score'] }} -
+                                    {{ $participants[0]['away_score'] }})</span>
+                            @endif
+                        </span>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endforeach
+</div>
