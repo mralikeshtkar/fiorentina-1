29f34a6d6b1e112d7b4475ccd6143a5985fe1646
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-24 13:37:39 +0200

    Reapply "1226"
    This reverts commit fe109ce55711a817d400c695f0e727b343ba80f5.

---
+++ node_modules/@botble/core/resources/lang/it/layouts.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'platform_admin' => 'Amministrazione della Piattaforma',
+    'dashboard' => 'Dashboard',
+    'widgets' => 'Widget',
+    'plugins' => 'Plugin',
+    'settings' => 'Impostazioni',
+    'setting_general' => 'Generale',
+    'setting_email' => 'Email',
+    'system_information' => 'Informazioni di Sistema',
+
+    'theme' => 'Tema',
+    'copyright' => 'Copyright :year © :company. Versione :version',
+    'profile' => 'Profilo',
+    'logout' => 'Logout',
+    'no_search_result' => 'Nessun risultato trovato, prova con parole chiave diverse.',
+    'home' => 'Home',
+    'search' => 'Cerca',
+    'add_new' => 'Aggiungi nuovo',
+    'n_a' => 'N/D',
+    'page_loaded_in_time' => 'Pagina caricata in :time secondi',
+    'view_website' => 'Visualizza sito',
+    'tools' => 'Strumenti',
+];
