b42aca6f02bedc9f6b9832ae9b0f3ffc979cc595
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-09-25 10:23:22 +0200

    Update diretta.blade.php

--- resources/views/ads/includes/diretta.blade.php
+++ resources/views/ads/includes/diretta.blade.php
@@ -98,7 +98,23 @@
     <div class="tab-content" id="myTabContent">
         <div class="tab-pane fade show active text-dark" id="formazioni" role="tabpanel"
             aria-labelledby="formazioni-tab">
-            @include('ads.includes.formazioni', ['groupedLineups' => $groupedLineups])
+            <ul class="nav nav-tabs mt-5" id="teamtab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link @if($isHomeFiorentina) active @endif" id="Home-tab" data-toggle="tab" href="#home" role="tab"
+                        aria-controls="home" aria-selected="false">Home</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link @if($isAwayFiorentina) active @endif" id="Away-tab" data-toggle="tab" href="#away" role="tab"
+                        aria-controls="away" aria-selected="false">Home</a>
+                </li>
+
+            </ul>
+            <div class="tab-pane fade" id="home" role="tabpanel" aria-labelledby="home-tab">
+                @include('ads.includes.formazioni', ['groupedLineups' => $groupedLineups])
+            </div>
+            <div class="tab-pane fade" id="away" role="tabpanel" aria-labelledby="away-tab">
+                Away
+            </div>
         </div>
         <div class="tab-pane fade" id="riassunto" role="tabpanel" aria-labelledby="riassunto-tab">
             @include('ads.includes.riassunto', ['summaries' => $summaries])
