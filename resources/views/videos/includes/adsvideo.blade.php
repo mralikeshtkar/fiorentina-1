7767054513b55e3877f908701cf860a191e6cb1f
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-09-30 09:29:08 +0200

    wertw

--- resources/views/videos/includes/adsvideo.blade.php
+++ resources/views/videos/includes/adsvideo.blade.php
@@ -10,7 +10,7 @@
             @foreach ($videos as $video)
                 <div class="col-12 col-md-12 col-lg-12 mx-auto">
                     <div class="d-block w-full">
-                        <video id="myVideo" width="100%" autoplay muted>
+                        <video width="100%" controls autoplay muted>
                             <source src="{{ asset('storage/' . $video->video_path) }}" type="video/mp4">
                             Your browser does not support the video tag.
                         </video>
