9870ae3942d07e41e8eb2fe5d65522d6c24c2b66
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-01 12:53:45 +0200

    Update addDiretta.blade.php

--- resources/views/diretta/includes/addDiretta.blade.php
+++ resources/views/diretta/includes/addDiretta.blade.php
@@ -43,4 +43,4 @@
         <button type="submit" class="btn btn-primary">Submit</button>
     </div>
 </form>
-@include('ads.includes.livecommentary')
+@include('ads.includes.commentary', ['commentaries' => $commentaries])
