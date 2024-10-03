669d3c70db22d9b38c862b78d6a70740ebbccc03
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-03 22:16:31 +0200

    Update livecommentary.blade.php

--- resources/views/ads/includes/livecommentary.blade.php
+++ resources/views/ads/includes/livecommentary.blade.php
@@ -3,15 +3,7 @@
 @endphp
 
 <div class="container mt-3" id="commentary-container">
-    @foreach ($commentaries as $comment)
-        <div
-            class="commentary-row {{ $comment['comment_class'] }} {{ $comment['is_important'] ? 'important' : '' }}{{ $comment['is_bold'] ? 'comment-is-bold' : '' }}">
-            <div class="comment-time">{{ $comment['comment_time'] }}</div>
-            <div class="comment-icon"></div>
-            <div class="comment-text {{ $comment['is_bold'] ? 'comment-bold' : '' }}">{{ $comment['comment_text'] }}
-            </div>
-        </div>
-    @endforeach
+
 </div>
 
 <script>
