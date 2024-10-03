2ca1d940e3d0b683e256a2631f270183d6904f1c
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-03 22:13:48 +0200

    Update MatchCommentaryController.php

--- app/Http/Controllers/MatchCommentaryController.php
+++ app/Http/Controllers/MatchCommentaryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\MatchCommentary;
 use App\Models\Calendario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
 class MatchCommentaryController extends Controller
