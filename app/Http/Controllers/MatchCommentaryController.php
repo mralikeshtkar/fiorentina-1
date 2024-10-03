0319fe7b088fe4c8b9e714c95db7f5495f59df03
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-03 22:13:21 +0200

    Update MatchCommentaryController.php

--- app/Http/Controllers/MatchCommentaryController.php
+++ app/Http/Controllers/MatchCommentaryController.php
@@ -12,8 +12,8 @@ class MatchCommentaryController extends Controller
 
     public function fetchLatestCommentaries($matchId)
 {
-    $data=$this->storeCommentaries($matchId);
-dd($data);
+$this->storeCommentaries($matchId);
+
     // Fetch the latest commentaries ordered by time
     $commentaries = MatchCommentary::where('match_id', $matchId)
         ->orderByRaw("
