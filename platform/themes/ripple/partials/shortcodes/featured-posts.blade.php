d8d2d2088a5cde2e261cb8fc402d5d2ca7d779a0
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-07 09:37:59 +0200

    0937

--- platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
+++ platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
@@ -3,7 +3,50 @@
     <section class="section hero-section pt-45 pb-20"
         @if ($shortcode->background_color) style="background-color: #441274 !important;" @endif>
 
+        @php
+            $match = App\Models\Calendario::where('status', 'SCHEDULED')
+                ->orWhere('status', 'LIVE')
+                ->orderBy('match_date', 'asc')
+                ->first();
+            $home_team = json_decode($match->home_team, true);
+            $away_team = json_decode($match->away_team, true);
 
+        @endphp
+
+        <div class="container mb-3">
+            <div class="row align-items-center upcoming-match">
+                <!-- Match Date, Time, and Venue -->
+                <div class="col-md-3">
+                    <p>{{ ucwords(\Carbon\Carbon::parse($match->match_date)->locale('it')->timezone('Europe/Rome')->isoFormat('dddd D MMMM [ore] H:mm')," \t\r\n\f\v") }}
+                    </p>
+                </div>
+
+                <!-- Team Logos and Names -->
+                <div class="col-md-6 text-center">
+                    <div class="row">
+                        <div class="col-6">
+                            <img src="{{ $home_team['logo'] }}" alt="{{ $home_team['name'] }} Crest"
+                                style="height: 30px; margin-bottom: 10px;">
+                            <h5>{{ $home_team['name'] }}</h5>
+                        </div>
+                        <div class="col-6">
+                            <img src="{{ $away_team['logo'] }}" alt="{{ $away_team['name'] }} Crest"
+                                style="height: 30px; margin-bottom: 10px;">
+                            <h5>{{ $away_team['name'] }}</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Ticket Buttons -->
+                <div class="col-md-3">
+                    <div class="d-grid">
+                        <a href="https://laviola.collaudo.biz/diretta?match_id={{ $match->match_id }}"
+                            class="btn-sm btn-primary mb-2 fiorentina-btn" style="grid-area: auto;">Vai alla
+                            diretta!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <div class="container">
             <div class="row">
@@ -47,9 +90,9 @@
                                 </div>
                             @endif
                         @endif
-                    @endforeach
-                    </div>
-                    </div>
+@endforeach
+</div>
+</div>
 
 <!-- Black box column (col-3) similar to the image -->
 <div class="col-md-3">
