fe511283825003a25f2cf3e32c4896addf4b7429
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-10-07 08:16:22 +0200

    rey

--- platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
+++ platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
@@ -13,40 +13,40 @@
 
 {{--        @endphp--}}
 
-        <div class="container mb-3">
-            <div class="row align-items-center upcoming-match">
-                <!-- Match Date, Time, and Venue -->
-                <div class="col-md-3">
-                    <p>{{ ucwords(\Carbon\Carbon::parse($match->match_date)->locale('it')->timezone('Europe/Rome')->isoFormat('dddd D MMMM [ore] H:mm')," \t\r\n\f\v") }}
-                    </p>
-                </div>
+{{--        <div class="container mb-3">--}}
+{{--            <div class="row align-items-center upcoming-match">--}}
+{{--                <!-- Match Date, Time, and Venue -->--}}
+{{--                <div class="col-md-3">--}}
+{{--                    <p>{{ ucwords(\Carbon\Carbon::parse($match->match_date)->locale('it')->timezone('Europe/Rome')->isoFormat('dddd D MMMM [ore] H:mm')," \t\r\n\f\v") }}--}}
+{{--                    </p>--}}
+{{--                </div>--}}
 
-                <!-- Team Logos and Names -->
-                <div class="col-md-6 text-center">
-                    <div class="row">
-                        <div class="col-6">
-                            <img src="{{ $match->home_team['logo'] }}" alt="{{ $match->home_team['name'] }} Crest"
-                                style="height: 30px; margin-bottom: 10px;">
-                            <h5>{{ $match->home_team['name'] }}</h5>
-                        </div>
-                        <div class="col-6">
-                            <img src="{{ $match->away_team['crest'] }}" alt="{{ $match->away_team['name'] }} Crest"
-                                style="height: 30px; margin-bottom: 10px;">
-                            <h5>{{ $match->away_team['name'] }}</h5>
-                        </div>
-                    </div>
-                </div>
+{{--                <!-- Team Logos and Names -->--}}
+{{--                <div class="col-md-6 text-center">--}}
+{{--                    <div class="row">--}}
+{{--                        <div class="col-6">--}}
+{{--                            <img src="{{ $match->home_team['logo'] }}" alt="{{ $match->home_team['name'] }} Crest"--}}
+{{--                                style="height: 30px; margin-bottom: 10px;">--}}
+{{--                            <h5>{{ $match->home_team['name'] }}</h5>--}}
+{{--                        </div>--}}
+{{--                        <div class="col-6">--}}
+{{--                            <img src="{{ $match->away_team['crest'] }}" alt="{{ $match->away_team['name'] }} Crest"--}}
+{{--                                style="height: 30px; margin-bottom: 10px;">--}}
+{{--                            <h5>{{ $match->away_team['name'] }}</h5>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
 
-                <!-- Ticket Buttons -->
-                <div class="col-md-3">
-                    <div class="d-grid">
-                        <a href="https://laviola.collaudo.biz/diretta?match_id={{ $match->match_id }}"
-                            class="btn-sm btn-primary mb-2 fiorentina-btn" style="grid-area: auto;">Vai alla
-                            diretta!</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+{{--                <!-- Ticket Buttons -->--}}
+{{--                <div class="col-md-3">--}}
+{{--                    <div class="d-grid">--}}
+{{--                        <a href="https://laviola.collaudo.biz/diretta?match_id={{ $match->match_id }}"--}}
+{{--                            class="btn-sm btn-primary mb-2 fiorentina-btn" style="grid-area: auto;">Vai alla--}}
+{{--                            diretta!</a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
         <div class="container">
             <div class="post-group post-group--hero">
                 @foreach ($posts as $post)
