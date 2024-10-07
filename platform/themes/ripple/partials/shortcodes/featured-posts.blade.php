b2744b54c8d2a7a3fe286d6fceae8b683ed50aa2
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-07 09:43:21 +0200

    Update featured-posts.blade.php

--- platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
+++ platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
@@ -96,50 +96,34 @@
 
 <!-- Black box column (col-3) similar to the image -->
 <div class="col-md-3">
+    @php
+        $lastRecentPosts = Botble\Blog\Models\Post::orderBy('created_at', 'desc')->take(4)->get();
+    @endphp
     <div class="black-box">
-        @foreach ($posts as $post)
-            @if ($loop->first)
-                <div class="post-group__left">
-                    <article class="post post__inside post__inside--feature">
-
-                        <header class="post__header">
-                            <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6>
-                            <h6 class="post__title text-truncate">
-                                <a href="{{ $post->url }}">{{ $post->name }}</a>
-                            </h6>
-                        </header>
-                    </article>
-                </div>
-                <div class="post-group__right">
-                @else
-                    <div class="post-group__item">
-                        <article class="post post__inside post__inside--feature post__inside--feature-small">
-
-                            <header class="post__header">
-                                <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6>
-                                <h6 class="post__title text-truncate">
-                                    <a href="{{ $post->url }}">{{ $post->name }}</a>
-                                </h6>
-                            </header>
-                        </article>
-                    </div>
-                    @if ($loop->last)
-                </div>
-            @endif
-        @endif
-        @endforeach
-        {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
-        {{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Fiorentina, il programma di oggi</p> --}}
-
-        {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
-        {{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Mutu: "Gud? Ho solo pensieri positivi, ma quando hai la 10 devi sempre dimostrare qualcosa in più"</p> --}}
-
-        {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
-        {{--                        <p style="font-size: 14px;color: white; margin-bottom: 20px;">Palladino (sala stampa): "Kean come un leone, De Gea è stato un fenomeno"</p> --}}
-
-        {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
-        {{--                        <p style="font-size: 14px;color: white; ">Commisso: "È stato un weekend perfetto. Domani parto, torno a gennaio"</p> --}}
+        @foreach ($lastRecentPosts as $post)
+            <article class="post post__inside post__inside--feature">
+                <header class="post__header">
+                    <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6>
+                    <h6 class="post__title text-truncate">
+                        <a href="{{ $post->url }}">{{ $post->name }}</a>
+                    </h6>
+                </header>
+            </article>
     </div>
+    @endif
+    @endforeach
+    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+    {{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Fiorentina, il programma di oggi</p> --}}
+
+    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+    {{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Mutu: "Gud? Ho solo pensieri positivi, ma quando hai la 10 devi sempre dimostrare qualcosa in più"</p> --}}
+
+    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+    {{--                        <p style="font-size: 14px;color: white; margin-bottom: 20px;">Palladino (sala stampa): "Kean come un leone, De Gea è stato un fenomeno"</p> --}}
+
+    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+    {{--                        <p style="font-size: 14px;color: white; ">Commisso: "È stato un weekend perfetto. Domani parto, torno a gennaio"</p> --}}
+</div>
 </div>
 </div>
 </div>
