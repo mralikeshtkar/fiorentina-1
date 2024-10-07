d0338dcdc2d302fe285fcf407378e45537dee113
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-07 09:05:06 +0200

    Update featured-posts.blade.php

--- platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
+++ platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
@@ -1,54 +1,51 @@
 @if ($posts->isNotEmpty())
 
     <section class="section hero-section pt-45 pb-20"
-        @if ($shortcode->background_color) style="background-color: #441274 !important;" @endif
-    >
-{{--        @php--}}
-{{--                    $match = App\Models\Calendario::where('status', 'SCHEDULED')--}}
-{{--                        ->orWhere('status', 'LIVE')--}}
-{{--                        ->orderBy('match_date', 'desc')--}}
-{{--                        ->firstOrFail();--}}
-{{--                    $match->home_team = json_decode($match->home_team, true);--}}
-{{--                    $match->away_team = json_decode($match->away_team, true);--}}
-{{--                    $match->odds = json_decode($match->odds, true);--}}
+        @if ($shortcode->background_color) style="background-color: #441274 !important;" @endif>
+        @php
+            $match = App\Models\Calendario::where('status', 'SCHEDULED')
+                ->orWhere('status', 'LIVE')
+                ->orderBy('match_date', 'desc')
+                ->first();
+            $match->home_team = json_decode($match->home_team, true);
+            $match->away_team = json_decode($match->away_team, true);
 
+        @endphp
 
-{{--                @endphp--}}
+        <div class="container mb-3">
+            <div class="row align-items-center upcoming-match">
+                <!-- Match Date, Time, and Venue -->
+                <div class="col-md-3">
+                    <p>{{ ucwords(\Carbon\Carbon::parse($match->match_date)->locale('it')->timezone('Europe/Rome')->isoFormat('dddd D MMMM [ore] H:mm')," \t\r\n\f\v") }}
+                    </p>
+                </div>
 
-{{--                <div class="container mb-3">--}}
-{{--            <div class="row align-items-center upcoming-match">--}}
-{{--                <!-- Match Date, Time, and Venue -->--}}
-{{--                <div class="col-md-3">--}}
-{{--                    <p>{{ ucwords(\Carbon\Carbon::parse($match->match_date)->locale('it')->timezone('Europe/Rome')->isoFormat('dddd D MMMM [ore] H:mm')," \t\r\n\f\v") }}--}}
-{{--                    </p>--}}
-{{--                </div>--}}
+                <!-- Team Logos and Names -->
+                <div class="col-md-6 text-center">
+                    <div class="row">
+                        <div class="col-6">
+                            <img src="{{ $match->home_team['logo'] }}" alt="{{ $match->home_team['name'] }} Crest"
+                                style="height: 30px; margin-bottom: 10px;">
+                            <h5>{{ $match->home_team['name'] }}</h5>
+                        </div>
+                        <div class="col-6">
+                            <img src="{{ $match->away_team['crest'] }}" alt="{{ $match->away_team['name'] }} Crest"
+                                style="height: 30px; margin-bottom: 10px;">
+                            <h5>{{ $match->away_team['name'] }}</h5>
+                        </div>
+                    </div>
+                </div>
 
-{{--                <!-- Team Logos and Names -->--}}
-{{--                <div class="col-md-6 text-center">--}}
-{{--                    <div class="row">--}}
-{{--                        <div class="col-6">--}}
-{{--                            <img src="{{ $match->home_team['logo'] }}" alt="{{ $match->home_team['name'] }} Crest"--}}
-{{--                                style="height: 30px; margin-bottom: 10px;">--}}
-{{--                            <h5>{{ $match->home_team['name'] }}</h5>--}}
-{{--                        </div>--}}
-{{--                        <div class="col-6">--}}
-{{--                            <img src="{{ $match->away_team['crest'] }}" alt="{{ $match->away_team['name'] }} Crest"--}}
-{{--                                style="height: 30px; margin-bottom: 10px;">--}}
-{{--                            <h5>{{ $match->away_team['name'] }}</h5>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-
-{{--                <!-- Ticket Buttons -->--}}
-{{--                <div class="col-md-3">--}}
-{{--                    <div class="d-grid">--}}
-{{--                        <a href="https://laviola.collaudo.biz/diretta?match_id={{ $match->match_id }}"--}}
-{{--                            class="btn-sm btn-primary mb-2 fiorentina-btn" style="grid-area: auto;">Vai alla--}}
-{{--                            diretta!</a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
+                <!-- Ticket Buttons -->
+                <div class="col-md-3">
+                    <div class="d-grid">
+                        <a href="https://laviola.collaudo.biz/diretta?match_id={{ $match->match_id }}"
+                            class="btn-sm btn-primary mb-2 fiorentina-btn" style="grid-area: auto;">Vai alla
+                            diretta!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="container">
             <div class="row">
                 <!-- Main content column (col-9) -->
@@ -60,7 +57,8 @@
                                     <article class="post post__inside post__inside--feature">
                                         <div class="post__thumbnail">
                                             {{ RvMedia::image($post->image, $post->name, 'featured', attributes: ['loading' => 'eager']) }}
-                                            <a class="post__overlay" href="{{ $post->url }}" title="{{ $post->name }}"></a>
+                                            <a class="post__overlay" href="{{ $post->url }}"
+                                                title="{{ $post->name }}"></a>
                                         </div>
                                         <header class="post__header">
                                             <h3 class="post__title text-truncate">
@@ -70,78 +68,80 @@
                                     </article>
                                 </div>
                                 <div class="post-group__right">
-                                    @else
-                                        <div class="post-group__item">
-                                            <article class="post post__inside post__inside--feature post__inside--feature-small">
-                                                <div class="post__thumbnail">
-                                                    {{ RvMedia::image($post->image, $post->name, 'medium', attributes: ['loading' => 'eager']) }}
-                                                    <a class="post__overlay" href="{{ $post->url }}" title="{{ $post->name }}"></a>
-                                                </div>
-                                                <header class="post__header">
-                                                    <h3 class="post__title text-truncate">
-                                                        <a href="{{ $post->url }}">{{ $post->name }}</a>
-                                                    </h3>
-                                                </header>
-                                            </article>
-                                        </div>
-                                        @if ($loop->last)
+                                @else
+                                    <div class="post-group__item">
+                                        <article
+                                            class="post post__inside post__inside--feature post__inside--feature-small">
+                                            <div class="post__thumbnail">
+                                                {{ RvMedia::image($post->image, $post->name, 'medium', attributes: ['loading' => 'eager']) }}
+                                                <a class="post__overlay" href="{{ $post->url }}"
+                                                    title="{{ $post->name }}"></a>
+                                            </div>
+                                            <header class="post__header">
+                                                <h3 class="post__title text-truncate">
+                                                    <a href="{{ $post->url }}">{{ $post->name }}</a>
+                                                </h3>
+                                            </header>
+                                        </article>
+                                    </div>
+                                    @if ($loop->last)
                                 </div>
                             @endif
-                            @endif
-                        @endforeach
-                    </div>
+                        @endif
+@endforeach
+</div>
+</div>
+
+<!-- Black box column (col-3) similar to the image -->
+<div class="col-md-3">
+    <div class="black-box">
+        @foreach ($posts as $post)
+            @if ($loop->first)
+                <div class="post-group__left">
+                    <article class="post post__inside post__inside--feature">
+
+                        <header class="post__header">
+                            <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6>
+                            <h6 class="post__title text-truncate">
+                                <a href="{{ $post->url }}">{{ $post->name }}</a>
+                            </h6>
+                        </header>
+                    </article>
                 </div>
+                <div class="post-group__right">
+                @else
+                    <div class="post-group__item">
+                        <article class="post post__inside post__inside--feature post__inside--feature-small">
 
-                <!-- Black box column (col-3) similar to the image -->
-                <div class="col-md-3">
-                    <div class="black-box" >
-                        @foreach ($posts as $post)
-                            @if ($loop->first)
-                                <div class="post-group__left">
-                                    <article class="post post__inside post__inside--feature">
-
-                                        <header class="post__header">
-                                            <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6>
-                                            <h6 class="post__title text-truncate">
-                                                <a href="{{ $post->url }}">{{ $post->name }}</a>
-                                            </h6>
-                                        </header>
-                                    </article>
-                                </div>
-                                <div class="post-group__right">
-                                    @else
-                                        <div class="post-group__item">
-                                            <article class="post post__inside post__inside--feature post__inside--feature-small">
-
-                                                <header class="post__header">
-                                                    <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6>
-                                                    <h6 class="post__title text-truncate">
-                                                        <a href="{{ $post->url }}">{{ $post->name }}</a>
-                                                    </h6>
-                                                </header>
-                                            </article>
-                                        </div>
-                                        @if ($loop->last)
-                                </div>
-                            @endif
-                            @endif
-                        @endforeach
-{{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6>--}}
-{{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Fiorentina, il programma di oggi</p>--}}
-
-{{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6>--}}
-{{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Mutu: "Gud? Ho solo pensieri positivi, ma quando hai la 10 devi sempre dimostrare qualcosa in più"</p>--}}
-
-{{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6>--}}
-{{--                        <p style="font-size: 14px;color: white; margin-bottom: 20px;">Palladino (sala stampa): "Kean come un leone, De Gea è stato un fenomeno"</p>--}}
-
-{{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6>--}}
-{{--                        <p style="font-size: 14px;color: white; ">Commisso: "È stato un weekend perfetto. Domani parto, torno a gennaio"</p>--}}
+                            <header class="post__header">
+                                <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6>
+                                <h6 class="post__title text-truncate">
+                                    <a href="{{ $post->url }}">{{ $post->name }}</a>
+                                </h6>
+                            </header>
+                        </article>
                     </div>
+                    @if ($loop->last)
                 </div>
-            </div>
-        </div>
+            @endif
+        @endif
+        @endforeach
+        {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+        {{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Fiorentina, il programma di oggi</p> --}}
+
+        {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+        {{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Mutu: "Gud? Ho solo pensieri positivi, ma quando hai la 10 devi sempre dimostrare qualcosa in più"</p> --}}
+
+        {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+        {{--                        <p style="font-size: 14px;color: white; margin-bottom: 20px;">Palladino (sala stampa): "Kean come un leone, De Gea è stato un fenomeno"</p> --}}
+
+        {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+        {{--                        <p style="font-size: 14px;color: white; ">Commisso: "È stato un weekend perfetto. Domani parto, torno a gennaio"</p> --}}
+    </div>
+</div>
+</div>
+</div>
 
 
-    </section>
+</section>
 @endif
