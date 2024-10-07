d40e7158fa2f6b05811ebc24f5dc06e854fae1d0
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-10-07 08:47:27 +0200

    dsfhgds

--- platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
+++ platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
@@ -3,52 +3,52 @@
     <section class="section hero-section pt-45 pb-20"
         @if ($shortcode->background_color) style="background-color: #441274 !important;" @endif
     >
-{{----}}        @php--}}
-        {{--            $match = App\Models\Calendario::where('status', 'SCHEDULED')--}}
-        {{--                ->orWhere('status', 'LIVE')--}}
-        {{--                ->orderBy('match_date', 'desc')--}}
-        {{--                ->firstOrFail();--}}
-        {{--            $match->home_team = json_decode($match->home_team, true);--}}
-        {{--            $match->away_team = json_decode($match->away_team, true);--}}
-        {{--            $match->odds = json_decode($match->odds, true);--}}
+        @php
+                    $match = App\Models\Calendario::where('status', 'SCHEDULED')
+                        ->orWhere('status', 'LIVE')
+                        ->orderBy('match_date', 'desc')
+                        ->firstOrFail();
+                    $match->home_team = json_decode($match->home_team, true);
+                    $match->away_team = json_decode($match->away_team, true);
+                    $match->odds = json_decode($match->odds, true);
 
 
-        {{--        @endphp
+                @endphp
 
-        {{--        <div class="container mb-3">--}}
-{{--            <div class="row align-items-center upcoming-match">--}}
-{{--                <!-- Match Date, Time, and Venue -->--}}
-{{--                <div class="col-md-3">--}}
-{{--                    <p>{{ ucwords(\Carbon\Carbon::parse($match->match_date)->locale('it')->timezone('Europe/Rome')->isoFormat('dddd D MMMM [ore] H:mm')," \t\r\n\f\v") }}--}}
-{{--                    </p>--}}
-{{--                </div>--}}
+                <div class="container mb-3">
+            <div class="row align-items-center upcoming-match">
+                <!-- Match Date, Time, and Venue -->
+                <div class="col-md-3">
+                    <p>{{ ucwords(\Carbon\Carbon::parse($match->match_date)->locale('it')->timezone('Europe/Rome')->isoFormat('dddd D MMMM [ore] H:mm')," \t\r\n\f\v") }}
+                    </p>
+                </div>
 
-{{--                <!-- Team Logos and Names -->--}}
-{{--                <div class="col-md-6 text-center">--}}
-{{--                    <div class="row">--}}
-{{--                        <div class="col-6">--}}
-{{--                            <img src="{{ $match->home_team['logo'] }}" alt="{{ $match->home_team['name'] }} Crest"--}}
-{{--                                style="height: 30px; margin-bottom: 10px;">--}}
-{{--                            <h5>{{ $match->home_team['name'] }}</h5>--}}
-{{--                        </div>--}}
-{{--                        <div class="col-6">--}}
-{{--                            <img src="{{ $match->away_team['crest'] }}" alt="{{ $match->away_team['name'] }} Crest"--}}
-{{--                                style="height: 30px; margin-bottom: 10px;">--}}
-{{--                            <h5>{{ $match->away_team['name'] }}</h5>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+                <!-- Team Logos and Names -->
+                <div class="col-md-6 text-center">
+                    <div class="row">
+                        <div class="col-6">
+                            <img src="{{ $match->home_team['logo'] }}" alt="{{ $match->home_team['name'] }} Crest"
+                                style="height: 30px; margin-bottom: 10px;">
+                            <h5>{{ $match->home_team['name'] }}</h5>
+                        </div>
+                        <div class="col-6">
+                            <img src="{{ $match->away_team['crest'] }}" alt="{{ $match->away_team['name'] }} Crest"
+                                style="height: 30px; margin-bottom: 10px;">
+                            <h5>{{ $match->away_team['name'] }}</h5>
+                        </div>
+                    </div>
+                </div>
 
-{{--                <!-- Ticket Buttons -->--}}
-{{--                <div class="col-md-3">--}}
-{{--                    <div class="d-grid">--}}
-{{--                        <a href="https://laviola.collaudo.biz/diretta?match_id={{ $match->match_id }}"--}}
-{{--                            class="btn-sm btn-primary mb-2 fiorentina-btn" style="grid-area: auto;">Vai alla--}}
-{{--                            diretta!</a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
+                <!-- Ticket Buttons -->
+                <div class="col-md-3">
+                    <div class="d-grid">
+                        <a href="https://laviola.collaudo.biz/diretta?match_id={{ $match->match_id }}"
+                            class="btn-sm btn-primary mb-2 fiorentina-btn" style="grid-area: auto;">Vai alla
+                            diretta!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="container">
             <div class="row">
                 <!-- Main content column (col-9) -->
