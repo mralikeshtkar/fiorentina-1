492241fc392aa13fc13d615b3bca69d5c46e94e0
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-10-14 10:56:41 +0200

    WET

--- resources/views/ads/includes/adsrecentp3.blade.php
+++ resources/views/ads/includes/adsrecentp3.blade.php
@@ -1,7 +1,7 @@
 @if(isset($ad) && $ad)
     <div class="row justify-content-center mx-0">
 
-        <div class="col-12 col-md-8 col-lg-6 mx-auto">
+        <div class="col-12 col-md-8 col-lg-12 mx-auto">
             <a href="" class="d-block">
                 <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid" style="width: 100%; height: auto;">
             </a>
