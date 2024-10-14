41e2718c02d081326211546380170438c276d8cc
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-10-14 13:30:37 +0200

    asf

--- resources/views/ads/create.blade.php
+++ resources/views/ads/create.blade.php
@@ -1,210 +1,55 @@
 @extends(BaseHelper::getAdminMasterLayoutTemplate())
 
 @section('content')
-    {{--    <form action="{{ route('ad.submit') }}" method="POST" enctype="multipart/form-data">--}}
     <form action="{{ route('ads.store') }}" method="POST" enctype="multipart/form-data">
-    @csrf <!-- CSRF Token for Laravel, ensures your form is secure -->
-
+        @csrf <!-- CSRF Token for Laravel, ensures your form is secure -->
 
         <div class="row">
-
             <div class="gap-3 col-md-9">
                 <div class="card mb-3">
                     <div class="card-body">
-                        <div class="metabox-holder columns-2" id="post-body">
-                            <!-- Title Section -->
-                            <div class="post-body-content">
-                                <div class="mb-3" id="titlediv">
-                                    <label for="title" class="form-label" id="title-prompt-text">Aggiungi titolo</label>
-                                    <input type="text" class="form-control" name="post_title" id="title"
-                                           value="{{ old('post_title') }}" spellcheck="true" autocomplete="off">
-                                </div>
-                            </div>
+                        <!-- Existing Form Sections -->
 
-                            <!-- Publish Section -->
-                            <div class="postbox-container" id="postbox-container-1">
-                                <div class="meta-box-sortables ui-sortable" id="side-sortables">
-                                    <div class="postbox" id="submitdiv">
-                                        <div class="postbox-header">
-                                            <h2 class="hndle ui-sortable-handle">Pubblica</h2>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <!-- Ad Type Selection -->
-                            <div class="postbox-container" id="postbox-container-2">
-                                <div class="meta-box-sortables ui-sortable" id="normal-sortables">
-                                    <div class="postbox" id="ad-main-box">
-                                        <div class="postbox-header">
-                                            <h2 class="hndle ui-sortable-handle">Tipo Annuncio:</h2>
-                                        </div>
-                                        <div class="inside">
-                                            <select class="form-select" name="type" id="advanced-ad-type">
-                                                @foreach(\App\Models\Ad::TYPES as $key => $title)
-                                                    <option
-                                                        value="{{ $key }}" @selected(old('type') == $key)>{{ $title }}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <!-- Image Upload Section -->
-                            <div class="row mb-3">
-                                <label for="imageUpload" class="form-label">Upload an image:</label>
-                                <input type="file" class="form-control" id="imageUpload" name="image" accept="image/*">
-                                <div class="row mx-0 mt-3">
-                                    <div class="col-12">
-                                        <img src="" class="image-preview" alt="">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <!-- Ad Parameters Section -->
-                            <div class="postbox" id="ad-parameters-box">
-                                <div class="postbox-header">
-                                    <h2 class="hndle ui-sortable-handle">Parametri annuncio</h2>
-                                </div>
-                                <div class="inside">
-                                    <label for="advads-group-id" class="form-label">Gruppo annunci</label>
-                                    <select class="form-select" name="group" id="advads-group-id">
-                                        @foreach(\App\Models\Ad::GROUPS as $key => $title)
-                                            <option
-                                                value="{{ $key }}" @selected(old('group') == $key)>{{ $title }}</option>
-                                        @endforeach
-                                    </select>
-
-                                    <div class="mt-3">
-                                        <label for="width" class="form-label">Larghezza (px)</label>
-                                        <input type="number" class="form-control" id="width" name="width"
-                                               value="{{ $ad_width ?? null }}">
-                                    </div>
-                                    <div class="mt-3">
-                                        <label for="height" class="form-label">Altezza (px)</label>
-                                        <input type="number" class="form-control" id="height" name="height"
-                                               value="{{ $ad_height ?? null }}">
-                                    </div>
-                                    <div class="form-check mt-3">
-                                        <input class="form-check-input" type="checkbox" id="advads-wrapper-add-sizes"
-                                               name="advanced_ad[output][add_wrapper_sizes]" value="true">
-                                        <label class="form-check-label" for="advads-wrapper-add-sizes">Prenota questo
-                                            spazio</label>
-                                    </div>
-                                </div>
+                        <!-- Video Upload and Playback Section -->
+                        <div class="row mb-3">
+                            <label for="videoUpload" class="form-label">Upload videos:</label>
+                            <input type="file" class="form-control" id="videoUpload" name="videos[]" accept="video/*" multiple>
+                            <div class="row mx-0 mt-3" id="video-container">
+                                <!-- Video previews will be injected here -->
                             </div>
                         </div>
 
-
+                        <!-- Delay Selection Section -->
+                        <div class="row mb-3">
+                            <label for="delaySelect" class="form-label">Select Delay Between Plays (ms):</label>
+                            <select class="form-select" id="delaySelect">
+                                <option value="1">1 ms</option>
+                                <option value="5">5 ms</option>
+                                <option value="10">10 ms</option>
+                                <option value="15">15 ms</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
-
             </div>
             <div class="col-md-3 gap-3 d-flex flex-column-reverse flex-md-column mb-md-0 mb-5">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="card-title">
-                            Publish
-                        </h4>
+                        <h4 class="card-title">Publish</h4>
                     </div>
                     <div class="card-body">
                         <div class="btn-list">
-                            <button class="btn btn-primary" type="submit" value="apply" name="submitter">
-                                <svg class="icon icon-left" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                     stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path
-                                        d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2"></path>
-                                    <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-                                    <path d="M14 4l0 4l-6 0l0 -4"></path>
-                                </svg>
-                                Save
-
-                            </button>
-
-                            <button class="btn" type="submit" name="submitter" value="save">
-                                <svg class="icon icon-left" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                     stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M4 18v3h16v-14l-8 -4l-8 4v3"></path>
-                                    <path d="M4 14h9"></path>
-                                    <path d="M10 11l3 3l-3 3"></path>
-                                </svg>
-                                Save &amp; Exit
-
-                            </button>
-
-
+                            <button class="btn btn-primary" type="submit" value="apply" name="submitter">Save</button>
+                            <button class="btn" type="submit" name="submitter" value="save">Save & Exit</button>
                         </div>
                     </div>
                 </div>
-
-                <div data-bb-waypoint="" data-bb-target="#form-actions"></div>
-
-                <header class="top-0 w-100 position-fixed end-0 z-1000 vertical-wrapper" id="form-actions"
-                        style="display: none;">
-                    <div class="navbar">
-                        <div class="container-xl">
-                            <div class="row g-2 align-items-center w-100">
-                                <div class="col">
-                                    <div class="page-pretitle">
-                                        <nav aria-label="breadcrumb">
-                                            <ol class="breadcrumb">
-                                            </ol>
-                                        </nav>
-
-                                    </div>
-                                </div>
-                                <div class="col-auto ms-auto d-print-none">
-                                    <div class="btn-list">
-                                        <button class="btn btn-primary" type="submit" value="apply" name="submitter">
-                                            <svg class="icon icon-left" xmlns="http://www.w3.org/2000/svg" width="24"
-                                                 height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                <path
-                                                    d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2"></path>
-                                                <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-                                                <path d="M14 4l0 4l-6 0l0 -4"></path>
-                                            </svg>
-                                            Save
-
-                                        </button>
-
-                                        <button class="btn" type="submit" name="submitter" value="save">
-                                            <svg class="icon icon-left" xmlns="http://www.w3.org/2000/svg" width="24"
-                                                 height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                <path d="M4 18v3h16v-14l-8 -4l-8 4v3"></path>
-                                                <path d="M4 14h9"></path>
-                                                <path d="M10 11l3 3l-3 3"></path>
-                                            </svg>
-                                            Save &amp; Exit
-
-                                        </button>
-
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </header>
-
-
+                <!-- Status Section -->
                 <div class="card meta-boxes">
                     <div class="card-header">
-                        <h4 class="card-title">
-                            <label for="status" class="form-label required">Status</label>
-                        </h4>
+                        <h4 class="card-title"><label for="status" class="form-label required">Status</label></h4>
                     </div>
-
-
-                    <div class=" card-body">
+                    <div class="card-body">
                         <select data-placeholder="Select an option" class="form-control form-select" required="required"
                                 id="status" name="status" aria-required="true">
                             <option value="published">Published</option>
@@ -217,17 +62,50 @@
         </div>
     </form>
 @endsection
+
 @push('footer')
     <script>
-        document.getElementById('imageUpload').addEventListener('change', function (e) {
+        document.getElementById('videoUpload').addEventListener('change', function (e) {
             const input = e.target;
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    $('.image-preview').attr('src', e.target.result);
-                }
-                reader.readAsDataURL(input.files[0]);
+            const videoContainer = document.getElementById('video-container');
+            videoContainer.innerHTML = ''; // Clear previous videos
+            const files = input.files;
+
+            for (let i = 0; i < files.length; i++) {
+                const videoElement = document.createElement('video');
+                videoElement.src = URL.createObjectURL(files[i]);
+                videoElement.controls = true;
+                videoElement.width = 320;
+                videoElement.height = 240;
+                videoElement.dataset.playNumber = i + 1;
+                videoContainer.appendChild(videoElement);
             }
-        })
+        });
+
+        function playVideosSequentially(delayInMilliseconds) {
+            const videos = document.querySelectorAll('#video-container video');
+            let currentIndex = 0;
+
+            function playNextVideo() {
+                if (currentIndex < videos.length) {
+                    const video = videos[currentIndex];
+                    video.play();
+                    currentIndex++;
+
+                    video.onended = function () {
+                        setTimeout(playNextVideo, delayInMilliseconds);
+                    };
+                }
+            }
+
+            playNextVideo();
+        }
+
+        // Trigger video sequence on some event, for example, after form submission
+        document.querySelector('form').addEventListener('submit', function (e) {
+            e.preventDefault(); // Prevent form submission for testing purposes
+            const delay = parseInt(document.getElementById('delaySelect').value); // Get the selected delay
+            playVideosSequentially(delay);
+        });
     </script>
 @endpush
