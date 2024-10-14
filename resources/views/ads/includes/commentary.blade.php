1dcd3d072010703d2257bdba615f9fc0cb5c5987
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-14 10:23:24 +0200

    Update commentary.blade.php

--- resources/views/ads/includes/commentary.blade.php
+++ resources/views/ads/includes/commentary.blade.php
@@ -7,9 +7,12 @@
             class="commentary-row {{ $comment['comment_class'] }} {{ $comment['is_important'] ? 'important' : '' }}{{ $comment['is_bold'] ? 'comment-is-bold' : '' }}">
             <div class="comment-time">{{ $comment['comment_time'] }}</div>
             <div>
-            @if (Str::contains(request()->url(), '/diretta/view'))
-                <a href="/delete-commentary?id={{ $comment->id }}"><i class="text-danger fa-solid fa-trash"></i></a>
-            @endif</div>
+                @if (Str::contains(request()->url(), '/diretta/view'))
+                    <a href="/delete-commentary?id={{ $comment->id }}"><i class="text-danger fa-solid fa-trash"></i></a>
+                    <a href="/modify-commentary?id={{ $comment->id }}"><i
+                            class="text-white fa-solid fa-pen-to-square"></i></i></a>
+                @endif
+            </div>
             <div class="comment-icon"></div>
             <div class="comment-text {{ $comment['is_bold'] ? 'comment-bold' : '' }}">{{ $comment['comment_text'] }}
             </div>
