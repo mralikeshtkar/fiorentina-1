a044e34f091bdba90c10e689e86b34c6ae9e802d
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-14 10:27:25 +0200

    Update commentary.blade.php

--- resources/views/ads/includes/commentary.blade.php
+++ resources/views/ads/includes/commentary.blade.php
@@ -8,7 +8,7 @@
             <div class="comment-time" style="flex: 0.5">{{ $comment['comment_time'] }}</div>
             <div style="flex: 0.5">
                 @if (Str::contains(request()->url(), '/diretta/view'))
-                    <a href="/delete-commentary?id={{ $comment->id }}"><i class="text-danger fa-solid fa-trash"></i></a>
+                    <a style="margin-right: 5px" href="/delete-commentary?id={{ $comment->id }}"><i class="text-danger fa-solid fa-trash"></i></a>
                     <a href="/modify-commentary?id={{ $comment->id }}"><i
                             class="text-white fa-solid fa-pen-to-square"></i></i></a>
                 @endif
