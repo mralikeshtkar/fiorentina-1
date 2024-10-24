c68f23e0fb64f9fac3c858a5091845580532d80d
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-24 13:00:03 +0200

    Update create.blade.php

--- resources/views/ads/create.blade.php
+++ resources/views/ads/create.blade.php
@@ -1,13 +1,10 @@
 @extends(BaseHelper::getAdminMasterLayoutTemplate())
 
 @section('content')
-    {{--    <form action="{{ route('ad.submit') }}" method="POST" enctype="multipart/form-data"> --}}
     <form action="{{ route('ads.store') }}" method="POST" enctype="multipart/form-data">
         @csrf <!-- CSRF Token for Laravel, ensures your form is secure -->
 
-
         <div class="row">
-
             <div class="gap-3 col-md-9">
                 <div class="card mb-3">
                     <div class="card-body">
@@ -52,7 +49,7 @@
                             </div>
 
                             <!-- Image Upload Section -->
-                            <div class="row mb-3">
+                            <div class="row mb-3" id="imageUploadSection">
                                 <label for="imageUpload" class="form-label">Upload an image:</label>
                                 <input type="file" class="form-control" id="imageUpload" name="image" accept="image/*">
                                 <div class="row mx-0 mt-3">
@@ -60,6 +57,13 @@
                                         <img src="" class="image-preview" alt="">
                                     </div>
                                 </div>
+                            </div>
+
+                            <!-- Image Name Section for Google Ad Manager -->
+                            <div class="row mb-3" id="googleAdImageNameSection" style="display: none;">
+                                <label for="imageName" class="form-label">Image Name:</label>
+                                <input type="text" class="form-control" id="imageName" name="image_name"
+                                    placeholder="XXXXXXXXX">
                             </div>
 
                             <!-- Ad Parameters Section -->
@@ -95,8 +99,6 @@
                                 </div>
                             </div>
                         </div>
-
-
                     </div>
                 </div>
 
@@ -111,90 +113,15 @@
                     <div class="card-body">
                         <div class="btn-list">
                             <button class="btn btn-primary" type="submit" value="apply" name="submitter">
-                                <svg class="icon icon-left" xmlns="http://www.w3.org/2000/svg" width="24"
-                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2"></path>
-                                    <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-                                    <path d="M14 4l0 4l-6 0l0 -4"></path>
-                                </svg>
                                 Save
-
                             </button>
 
                             <button class="btn" type="submit" name="submitter" value="save">
-                                <svg class="icon icon-left" xmlns="http://www.w3.org/2000/svg" width="24"
-                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M4 18v3h16v-14l-8 -4l-8 4v3"></path>
-                                    <path d="M4 14h9"></path>
-                                    <path d="M10 11l3 3l-3 3"></path>
-                                </svg>
                                 Save &amp; Exit
-
                             </button>
-
-
                         </div>
                     </div>
                 </div>
-
-                <div data-bb-waypoint="" data-bb-target="#form-actions"></div>
-
-                <header class="top-0 w-100 position-fixed end-0 z-1000 vertical-wrapper" id="form-actions"
-                    style="display: none;">
-                    <div class="navbar">
-                        <div class="container-xl">
-                            <div class="row g-2 align-items-center w-100">
-                                <div class="col">
-                                    <div class="page-pretitle">
-                                        <nav aria-label="breadcrumb">
-                                            <ol class="breadcrumb">
-                                            </ol>
-                                        </nav>
-
-                                    </div>
-                                </div>
-                                <div class="col-auto ms-auto d-print-none">
-                                    <div class="btn-list">
-                                        <button class="btn btn-primary" type="submit" value="apply" name="submitter">
-                                            <svg class="icon icon-left" xmlns="http://www.w3.org/2000/svg" width="24"
-                                                height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                <path
-                                                    d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2">
-                                                </path>
-                                                <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-                                                <path d="M14 4l0 4l-6 0l0 -4"></path>
-                                            </svg>
-                                            Save
-
-                                        </button>
-
-                                        <button class="btn" type="submit" name="submitter" value="save">
-                                            <svg class="icon icon-left" xmlns="http://www.w3.org/2000/svg" width="24"
-                                                height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                <path d="M4 18v3h16v-14l-8 -4l-8 4v3"></path>
-                                                <path d="M4 14h9"></path>
-                                                <path d="M10 11l3 3l-3 3"></path>
-                                            </svg>
-                                            Save &amp; Exit
-
-                                        </button>
-
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </header>
-
 
                 <div class="card meta-boxes">
                     <div class="card-header">
@@ -203,10 +130,9 @@
                         </h4>
                     </div>
 
-
                     <div class=" card-body">
-                        <select data-placeholder="Select an option" class="form-control form-select" required="required"
-                            id="status" name="status" aria-required="true">
+                        <select class="form-control form-select" required="required" id="status" name="status"
+                            aria-required="true">
                             <option value="published">Published</option>
                             <option value="draft">Draft</option>
                             <option value="pending">Pending</option>
@@ -217,19 +143,8 @@
         </div>
     </form>
 @endsection
+
 @push('footer')
-    <script>
-        document.getElementById('imageUpload').addEventListener('change', function(e) {
-            const input = e.target;
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    $('.image-preview').attr('src', e.target.result);
-                }
-                reader.readAsDataURL(input.files[0]);
-            }
-        })
-    </script>
     <script>
         document.getElementById('advanced-ad-type').addEventListener('change', function(e) {
             const selectedAdType = e.target.value;
