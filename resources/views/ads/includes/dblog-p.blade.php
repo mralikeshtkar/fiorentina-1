72cf1508a01bffd47ba2d6d263ef346d69a899a1
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-24 13:24:20 +0200

    1324

--- resources/views/ads/includes/dblog-p.blade.php
+++ resources/views/ads/includes/dblog-p.blade.php
@@ -1,14 +1,17 @@
-@if(isset($ad) && $ad)
-    <div class="row mx-0">
+@if (isset($ad) && $ad)
+    @if ($ad->type == 1)
+        <div class="row justify-content-center mx-0">
 
-            <div class="col-12 ">
+            <div class="col-12 col-md-8 col-lg-6 mx-auto">
                 <a href="" class="d-block">
-                    <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid" style="width: 100%; height: auto;">
+                    <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
+                        style="width: 100%; height: auto;">
                 </a>
             </div>
 
-    </div>
+        </div>
+    @else
+        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6741446998584415"
+            data-ad-slot="{{ $ad->image }}" data-ad-format="auto" data-full-width-responsive="true"></ins>
+    @endif
 @endif
-
-
-
--- resources/views/ads/includes/main-page.blade.php
+++ resources/views/ads/includes/main-page.blade.php
@@ -2,11 +2,21 @@
     <div class="container">
         <div class="row mx-0">
             @foreach($ads as $ad)
-                <div class="col-12 col-md-8 col-lg-6 mx-auto">
-                    <a href="" class="d-block w-full">
-                        <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="w-100">
-                    </a>
-                </div>
+            @if ($ad->type == 1)
+                    <div class="row justify-content-center mx-0">
+            
+                        <div class="col-12 col-md-8 col-lg-6 mx-auto">
+                            <a href="" class="d-block">
+                                <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
+                                    style="width: 100%; height: auto;">
+                            </a>
+                        </div>
+            
+                    </div>
+                @else
+                    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6741446998584415"
+                        data-ad-slot="{{ $ad->image }}" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                @endif
             @endforeach
         </div>
     </div>
