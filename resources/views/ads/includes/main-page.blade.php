ac59cdcebcba776ac2ec9478032a7c18267b1a04
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-25 12:54:38 +0200

    Update main-page.blade.php

--- resources/views/ads/includes/main-page.blade.php
+++ resources/views/ads/includes/main-page.blade.php
@@ -6,7 +6,7 @@
                     <div class="row justify-content-center mx-0">
 
                         <div class="col-12 mx-auto">
-                            <a href="" class="d-block">
+                            <a href="{{ $ad->url }}" class="d-block">
                                 <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
                                     style="width: 100%; height: auto;">
                             </a>
