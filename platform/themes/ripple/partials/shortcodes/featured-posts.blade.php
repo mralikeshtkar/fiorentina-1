720e66bf074244a78887d91dd979eed6d86ee4c6
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-25 14:18:19 +0200

    Update featured-posts.blade.php

--- platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
+++ platform/themes/ripple/partials/shortcodes/featured-posts.blade.php
@@ -1,7 +1,7 @@
 @if ($posts->isNotEmpty())
 
     <section class="section hero-section pt-45 pb-20"
-             @if ($shortcode->background_color) style="background-color: #441274 !important;" @endif>
+        @if ($shortcode->background_color) style="background-color: #441274 !important;" @endif>
 
         @php
             $match = App\Models\Calendario::where('status', 'SCHEDULED')
@@ -12,7 +12,7 @@
             $away_team = json_decode($match->away_team, true);
 
         @endphp
-        <div class="container">
+        <div class="container mb-3">
             <div class="row">
                 <div class="col-6 mx-auto">
                     <div class="col-11">
@@ -26,7 +26,7 @@
                 </div>
             </div>
         </div>
-        
+
         <div class="container mb-3 ">
             <div class="row align-items-center upcoming-match">
                 <!-- Match Date, Time, and Venue -->
@@ -40,12 +40,12 @@
                     <div class="row">
                         <div class="col-6">
                             <img src="{{ $home_team['logo'] }}" alt="{{ $home_team['name'] }} Crest"
-                                 style="height: 30px; margin-bottom: 10px;">
+                                style="height: 30px; margin-bottom: 10px;">
                             <h5>{{ $home_team['name'] }}</h5>
                         </div>
                         <div class="col-6">
                             <img src="{{ $away_team['logo'] }}" alt="{{ $away_team['name'] }} Crest"
-                                 style="height: 30px; margin-bottom: 10px;">
+                                style="height: 30px; margin-bottom: 10px;">
                             <h5>{{ $away_team['name'] }}</h5>
                         </div>
                     </div>
@@ -55,7 +55,7 @@
                 <div class="col-md-3">
                     <div class="d-grid">
                         <a href="https://laviola.collaudo.biz/diretta?match_id={{ $match->match_id }}"
-                           class="btn-sm btn-primary mb-2 fiorentina-btn" style="grid-area: auto;">Vai alla
+                            class="btn-sm btn-primary mb-2 fiorentina-btn" style="grid-area: auto;">Vai alla
                             diretta!</a>
                     </div>
                 </div>
@@ -74,12 +74,13 @@
                                         <div class="post__thumbnail h-100">
                                             {{ RvMedia::image($post->image, $post->name, 'featured', attributes: ['loading' => 'eager']) }}
                                             <a class="post__overlay" href="{{ $post->url }}"
-                                               title="{{ $post->name }}"></a>
+                                                title="{{ $post->name }}"></a>
                                         </div>
                                         <header class="post__header">
-                                            @if($post->categories->count())
+                                            @if ($post->categories->count())
                                                 <div class="d-flex">
-                                                    <span class="post-group__left-purple-badge mb-2">{{ $post->categories->first()->name }}</span>
+                                                    <span
+                                                        class="post-group__left-purple-badge mb-2">{{ $post->categories->first()->name }}</span>
                                                 </div>
                                             @endif
                                             <h3 class="post__title">
@@ -89,74 +90,77 @@
                                     </article>
                                 </div>
                                 <div class="post-group__right d-flex flex-column">
-                                    @else
-                                        <div class="post-group__item w-100 flex-grow-1">
-                                            <article
-                                                class="post post__inside post__inside--feature post__inside--feature-small h-100">
-                                                <div class="post__thumbnail h-100">
-                                                    {{ RvMedia::image($post->image, $post->name, 'medium', attributes: ['loading' => 'eager']) }}
-                                                    <a class="post__overlay" href="{{ $post->url }}"
-                                                       title="{{ $post->name }}"></a>
-                                                </div>
-                                                <header class="post__header">
-                                                    @if($post->categories->count())
-                                                        <div class="d-flex">
-                                                            <span class="fz-14px post-group__left-purple-badge">{{ $post->categories->first()->name }}</span>
-                                                        </div>
-                                                    @endif
-                                                    <h3 class="post__title">
-                                                        <a href="{{ $post->url }}">{{ $post->name }}</a>
-                                                    </h3>
-                                                </header>
-                                            </article>
-                                        </div>
-                                        @if ($loop->last)
+                                @else
+                                    <div class="post-group__item w-100 flex-grow-1">
+                                        <article
+                                            class="post post__inside post__inside--feature post__inside--feature-small h-100">
+                                            <div class="post__thumbnail h-100">
+                                                {{ RvMedia::image($post->image, $post->name, 'medium', attributes: ['loading' => 'eager']) }}
+                                                <a class="post__overlay" href="{{ $post->url }}"
+                                                    title="{{ $post->name }}"></a>
+                                            </div>
+                                            <header class="post__header">
+                                                @if ($post->categories->count())
+                                                    <div class="d-flex">
+                                                        <span
+                                                            class="fz-14px post-group__left-purple-badge">{{ $post->categories->first()->name }}</span>
+                                                    </div>
+                                                @endif
+                                                <h3 class="post__title">
+                                                    <a href="{{ $post->url }}">{{ $post->name }}</a>
+                                                </h3>
+                                            </header>
+                                        </article>
+                                    </div>
+                                    @if ($loop->last)
                                 </div>
                             @endif
-                            @endif
-                        @endforeach
-                    </div>
-                </div>
+                        @endif
+@endforeach
+</div>
+</div>
 
-                <!-- Black box column (col-3) similar to the image -->
-                <div class="col-12 col-lg-3 mx-0 px-0">
-                    @php
-                        $lastRecentPosts = Botble\Blog\Models\Post::with('categories:id,name')->orderBy('created_at', 'desc')->take(4)->get();
-                    @endphp
-                    <div class="black-box px-3 py-3">
-                        <div class="d-flex flex-column justify-content-around h-100">
-                            @foreach ($lastRecentPosts as $post)
-                                <article class="w-100 @unless($loop->last) mb-3 @endunless">
-                                    <header class="post__last4">
-                                        @if($post->categories->count())
-                                            <div class="d-flex mb-1">
-                            <span class="post__last4-badge">
-                            {{ $post->categories->first()->name }}</span>
-                                            </div>
-                                        @endif
-                                        <a class="post__last4-text"
-                                           href="{{ $post->url }}">{{ $post->name }}</a>
-                                    </header>
-                                </article>
-                            @endforeach
-                        </div>
-                    </div>
-                    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
-                    {{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Fiorentina, il programma di oggi</p> --}}
-
-                    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
-                    {{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Mutu: "Gud? Ho solo pensieri positivi, ma quando hai la 10 devi sempre dimostrare qualcosa in più"</p> --}}
-
-                    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
-                    {{--                        <p style="font-size: 14px;color: white; margin-bottom: 20px;">Palladino (sala stampa): "Kean come un leone, De Gea è stato un fenomeno"</p> --}}
-
-                    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
-                    {{--                        <p style="font-size: 14px;color: white; ">Commisso: "È stato un weekend perfetto. Domani parto, torno a gennaio"</p> --}}
-                </div>
-            </div>
+<!-- Black box column (col-3) similar to the image -->
+<div class="col-12 col-lg-3 mx-0 px-0">
+    @php
+        $lastRecentPosts = Botble\Blog\Models\Post::with('categories:id,name')
+            ->orderBy('created_at', 'desc')
+            ->take(4)
+            ->get();
+    @endphp
+    <div class="black-box px-3 py-3">
+        <div class="d-flex flex-column justify-content-around h-100">
+            @foreach ($lastRecentPosts as $post)
+                <article class="w-100 @unless ($loop->last) mb-3 @endunless">
+                    <header class="post__last4">
+                        @if ($post->categories->count())
+                            <div class="d-flex mb-1">
+                                <span class="post__last4-badge">
+                                    {{ $post->categories->first()->name }}</span>
+                            </div>
+                        @endif
+                        <a class="post__last4-text" href="{{ $post->url }}">{{ $post->name }}</a>
+                    </header>
+                </article>
+            @endforeach
         </div>
-        </div>
+    </div>
+    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+    {{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Fiorentina, il programma di oggi</p> --}}
+
+    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+    {{--                        <p style="font-size: 14px;color: white;  margin-bottom: 20px;">Mutu: "Gud? Ho solo pensieri positivi, ma quando hai la 10 devi sempre dimostrare qualcosa in più"</p> --}}
+
+    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+    {{--                        <p style="font-size: 14px;color: white; margin-bottom: 20px;">Palladino (sala stampa): "Kean come un leone, De Gea è stato un fenomeno"</p> --}}
+
+    {{--                        <h6 style="color: grey; font-size: 10px; margin-bottom: 5px;">NOTIZIE</h6> --}}
+    {{--                        <p style="font-size: 14px;color: white; ">Commisso: "È stato un weekend perfetto. Domani parto, torno a gennaio"</p> --}}
+</div>
+</div>
+</div>
+</div>
 
 
-    </section>
+</section>
 @endif
