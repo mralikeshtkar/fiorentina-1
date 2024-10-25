7e6d98a880a237664f964b2b92a4650f772f37f9
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-25 14:13:02 +0200

    Update SIZE_468X60_TOP_DX.blade.php

--- resources/views/ads/includes/SIZE_468X60_TOP_DX.blade.php
+++ resources/views/ads/includes/SIZE_468X60_TOP_DX.blade.php
@@ -1,21 +1,19 @@
-@if (isset($ads) && $ads)
+@if (isset($ad) && $ad)
     <div class="container">
         <div class="row mx-0">
-            @foreach ($ads as $ad)
-                @if ($ad->type == 1)
-                    <div class="row justify-content-center mx-0">
+            @if ($ad->type == 1)
+                <div class="row justify-content-center mx-0">
 
-                        <div class="col-8 mx-auto">
-                            <a href="{{ $ad->url }}" class="d-block">
-                                <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
-                                    style="width: 100%; height: auto;">
-                            </a>
-                        </div>
+                    <div class="col-8 mx-auto">
+                        <a href="{{ $ad->url }}" class="d-block">
+                            <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
+                                style="width: 100%; height: auto;">
+                        </a>
                     </div>
-                @else
-                    {!! $ad->amp !!}
-                @endif
-            @endforeach
+                </div>
+            @else
+                {!! $ad->amp !!}
+            @endif
         </div>
     </div>
 @endif
