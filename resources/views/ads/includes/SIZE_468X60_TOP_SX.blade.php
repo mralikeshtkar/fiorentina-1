846d84f7534cdf6c8b2b514832429ddb13f08355
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-10-25 14:09:18 +0200

    1409

--- resources/views/ads/includes/SIZE_468X60_TOP_SX.blade.php
+++ resources/views/ads/includes/SIZE_468X60_TOP_SX.blade.php
@@ -1,1 +1,21 @@
-gooogleads
+@if (isset($ads) && $ads)
+    <div class="container">
+        <div class="row mx-0">
+            @foreach ($ads as $ad)
+                @if ($ad->type == 1)
+                    <div class="row justify-content-center mx-0">
+
+                        <div class="col-8 mx-auto">
+                            <a href="{{ $ad->url }}" class="d-block">
+                                <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
+                                    style="width: 100%; height: auto;">
+                            </a>
+                        </div>
+                    </div>
+                @else
+                    {!! $ad->amp !!}
+                @endif
+            @endforeach
+        </div>
+    </div>
+@endif
